2314e7bd2868cfc027bfc57e9050aebbfcc21200
keeptxred <freddy.coppola@gmail.com>
2026-08-04 07:30:46 -0500

    seo: add active knowledge graph entities to sitemap

--- src/routes/sitemap[.]xml.ts
+++ src/routes/sitemap[.]xml.ts
@@ -2,6 +2,8 @@ import { createFileRoute } from "@tanstack/react-router";
 
 import { texasDefinedBrand } from "@/brand/texasdefined";
 import { platform, scope } from "@/data";
+import { loadTexasKnowledgeGraph } from "@/data/knowledge-graph";
+import { canonicalEntityPath } from "@/data/knowledge-graph/relationships";
 
 const origin = `https://${texasDefinedBrand.identity.domain}`;
 
@@ -9,37 +11,23 @@ export const Route = createFileRoute("/sitemap.xml")({
   server: {
     handlers: {
       GET: async () => {
-        const [articles, destinations, collections, categories] = await Promise.all([
+        const [articles, destinations, collections, categories, graph] = await Promise.all([
           platform.articles.list(scope),
           platform.destinations.list(scope),
           platform.collections.list(scope),
           platform.taxonomy.categories(scope),
+          loadTexasKnowledgeGraph(),
         ]);
 
         const paths = [
-          "/",
-          "/explore",
-          "/sports",
-          "/events",
-          "/texas-history",
-          "/moving-to-texas",
-          "/home-garden",
-          "/real-estate",
-          "/guides",
-          "/learn/property-taxes",
-          "/learn/property-tax-payments",
-          "/learn/appraisal-districts",
-          "/decide/property-taxes",
-          "/do/homestead-exemption",
-          "/do/property-tax-protest",
-          "/browse/counties",
-          "/browse/cities",
-          "/shop",
-          "/about",
+          "/", "/explore", "/sports", "/events", "/texas-history", "/moving-to-texas", "/home-garden", "/real-estate", "/guides",
+          "/learn/property-taxes", "/learn/property-tax-payments", "/learn/appraisal-districts", "/decide/property-taxes", "/do/homestead-exemption", "/do/property-tax-protest", "/browse/counties", "/browse/cities",
+          "/shop", "/about",
           ...categories.map((category) => `/explore/${category.slug}`),
           ...collections.map((collection) => `/shop/${collection.slug}`),
           ...articles.map((article) => `/article/${article.slug}`),
           ...destinations.map((destination) => `/destination/${destination.slug}`),
+          ...graph.filter((entity) => entity.status === 'active' || entity.status === 'seasonal').map(canonicalEntityPath),
         ];
 
         const uniquePaths = [...new Set(paths)];
@@ -48,12 +36,7 @@ export const Route = createFileRoute("/sitemap.xml")({
 ${uniquePaths.map((path) => `  <url><loc>${origin}${path}</loc></url>`).join("\n")}
 </urlset>`;
 
-        return new Response(body, {
-          headers: {
-            "content-type": "application/xml; charset=utf-8",
-            "cache-control": "public, max-age=3600",
-          },
-        });
+        return new Response(body, { headers: { "content-type": "application/xml; charset=utf-8", "cache-control": "public, max-age=3600" } });
       },
     },
   },
