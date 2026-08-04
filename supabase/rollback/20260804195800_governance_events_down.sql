9045e2061d19ce1c54238aad142b747719b80c46
keeptxred <freddy.coppola@gmail.com>
2026-08-04 15:02:53 -0500

    chore: complete governance persistence rollback

--- supabase/rollback/20260804195800_governance_events_down.sql
+++ supabase/rollback/20260804195800_governance_events_down.sql
@@ -1,2 +1,4 @@
+drop function if exists public.refresh_platform_governance_daily_summaries(integer);
 drop function if exists public.prune_platform_governance_events(integer);
+drop table if exists public.platform_governance_daily_summaries;
 drop table if exists public.platform_governance_events;
