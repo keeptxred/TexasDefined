271249451b01cb18e4a0337b9048f6ad3d947e77
keeptxred <freddy.coppola@gmail.com>
2026-08-05 11:53:42 -0500

    feat: restore dedicated major springs route

--- src/routes/explore.major-springs.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute, redirect } from '@tanstack/react-router';
-
-export const Route = createFileRoute('/explore/major-springs')({
-  beforeLoad: () => {
-    throw redirect({ href: '/explore/lakes-rivers', statusCode: 301 });
-  },
-});
