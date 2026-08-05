a89d6752884c79ce598d14c0ceb3d41797a07b9c
keeptxred <freddy.coppola@gmail.com>
2026-08-05 13:40:37 -0500

    seo: preserve queries on spring conservation redirect

--- src/routes/explore.spring-conservation-and-education.tsx
+++ src/routes/explore.spring-conservation-and-education.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/explore/spring-conservation-and-education')({
-  beforeLoad: () => {
-    throw redirect({ href: '/explore/lakes-rivers', statusCode: 301 });
+  beforeLoad: ({ location }) => {
+    throw redirect({ href: `/explore/lakes-rivers${location.searchStr || ''}`, statusCode: 301 });
   },
 });
