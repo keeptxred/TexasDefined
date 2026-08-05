67eb985574ab8908b986d34165a210023e6216a4
keeptxred <freddy.coppola@gmail.com>
2026-08-05 11:02:32 -0500

    make pagination language more editorial

--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -7,7 +7,7 @@ import { ButtonProps, buttonVariants } from "@/components/ui/button";
 const Pagination = ({ className, ...props }: React.ComponentProps<"nav">) => (
   <nav
     role="navigation"
-    aria-label="pagination"
+    aria-label="Story pages"
     className={cn("mx-auto flex w-full justify-center", className)}
     {...props}
   />
@@ -51,25 +51,25 @@ const PaginationPrevious = ({
   ...props
 }: React.ComponentProps<typeof PaginationLink>) => (
   <PaginationLink
-    aria-label="Go to previous page"
+    aria-label="See earlier stories"
     size="default"
     className={cn("gap-1 pl-2.5", className)}
     {...props}
   >
     <ChevronLeft className="h-4 w-4" />
-    <span>Previous</span>
+    <span>Earlier stories</span>
   </PaginationLink>
 );
 PaginationPrevious.displayName = "PaginationPrevious";
 
 const PaginationNext = ({ className, ...props }: React.ComponentProps<typeof PaginationLink>) => (
   <PaginationLink
-    aria-label="Go to next page"
+    aria-label="See more stories"
     size="default"
     className={cn("gap-1 pr-2.5", className)}
     {...props}
   >
-    <span>Next</span>
+    <span>More stories</span>
     <ChevronRight className="h-4 w-4" />
   </PaginationLink>
 );
@@ -82,7 +82,7 @@ const PaginationEllipsis = ({ className, ...props }: React.ComponentProps<"span"
     {...props}
   >
     <MoreHorizontal className="h-4 w-4" />
-    <span className="sr-only">More pages</span>
+    <span className="sr-only">More story pages</span>
   </span>
 );
 PaginationEllipsis.displayName = "PaginationEllipsis";
