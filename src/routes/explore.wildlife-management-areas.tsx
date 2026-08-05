d76a32b356a6087bbeb42f1fb9ad69f1b04b5f38
keeptxred <freddy.coppola@gmail.com>
2026-08-05 13:40:01 -0500

    seo: preserve queries on wildlife area redirect

--- src/routes/explore.wildlife-management-areas.tsx
+++ src/routes/explore.wildlife-management-areas.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/explore/wildlife-management-areas')({
-  beforeLoad: () => {
-    throw redirect({ href: '/explore/outdoors', statusCode: 301 });
+  beforeLoad: ({ location }) => {
+    throw redirect({ href: `/explore/outdoors${location.searchStr || ''}`, statusCode: 301 });
   },
 });
