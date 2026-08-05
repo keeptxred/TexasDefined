6b0a2171afb13e000275e80a02b5d4c69d15c615
keeptxred <freddy.coppola@gmail.com>
2026-08-05 11:44:49 -0500

    Remove duplicate historic sites redirect

--- src/routes/explore.historic-sites.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute, redirect } from '@tanstack/react-router';
-
-export const Route = createFileRoute('/explore/historic-sites')({
-  beforeLoad: () => {
-    throw redirect({ href: '/explore/historic-sites', statusCode: 301 });
-  },
-});
