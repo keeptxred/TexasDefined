56bd1c49073bfe8f7e8053ba1b5d72e5979666ef
keeptxred <freddy.coppola@gmail.com>
2026-08-05 13:40:12 -0500

    seo: preserve queries on lighthouse redirect

--- src/routes/explore.lighthouses.tsx
+++ src/routes/explore.lighthouses.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/explore/lighthouses')({
-  beforeLoad: () => {
-    throw redirect({ href: '/explore/beaches-coast', statusCode: 301 });
+  beforeLoad: ({ location }) => {
+    throw redirect({ href: `/explore/beaches-coast${location.searchStr || ''}`, statusCode: 301 });
   },
 });
