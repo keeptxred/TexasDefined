05633658774dcad15e776f5a3dfc3c206bbda675
keeptxred <freddy.coppola@gmail.com>
2026-08-07 17:46:14 -0500

    Wire sixth destination curation batch

--- src/data/destination-curation-all.ts
+++ src/data/destination-curation-all.ts
@@ -3,24 +3,12 @@ import { applyCuratedDestinationBatch2, applyCuratedDestinationsBatch2 } from ".
 import { applyCuratedDestinationBatch3, applyCuratedDestinationsBatch3 } from "./destination-curation-batch3";
 import { applyCuratedDestinationBatch4, applyCuratedDestinationsBatch4 } from "./destination-curation-batch4";
 import { applyCuratedDestinationBatch5, applyCuratedDestinationsBatch5 } from "./destination-curation-batch5";
+import { applyCuratedDestinationBatch6, applyCuratedDestinationsBatch6 } from "./destination-curation-batch6";
 import type { Destination } from "./types";
 
 export function applyAllCuratedDestination(destination: Destination): Destination {
-  return applyCuratedDestinationBatch5(
-    applyCuratedDestinationBatch4(
-      applyCuratedDestinationBatch3(
-        applyCuratedDestinationBatch2(applyCuratedDestination(destination)),
-      ),
-    ),
-  );
+  return applyCuratedDestinationBatch6(applyCuratedDestinationBatch5(applyCuratedDestinationBatch4(applyCuratedDestinationBatch3(applyCuratedDestinationBatch2(applyCuratedDestination(destination))))));
 }
-
 export function applyAllCuratedDestinations(destinations: Destination[]): Destination[] {
-  return applyCuratedDestinationsBatch5(
-    applyCuratedDestinationsBatch4(
-      applyCuratedDestinationsBatch3(
-        applyCuratedDestinationsBatch2(applyCuratedDestinations(destinations)),
-      ),
-    ),
-  );
+  return applyCuratedDestinationsBatch6(applyCuratedDestinationsBatch5(applyCuratedDestinationsBatch4(applyCuratedDestinationsBatch3(applyCuratedDestinationsBatch2(applyCuratedDestinations(destinations))))));
 }
