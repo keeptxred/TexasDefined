1c5629029ce0f75fea3408d439006824c57c2369
keeptxred <freddy.coppola@gmail.com>
2026-08-07 17:41:27 -0500

    Wire destination curation batch 4

--- src/data/destination-curation-all.ts
+++ src/data/destination-curation-all.ts
@@ -1,16 +1,21 @@
 import { applyCuratedDestination, applyCuratedDestinations } from "./destination-curation";
 import { applyCuratedDestinationBatch2, applyCuratedDestinationsBatch2 } from "./destination-curation-batch2";
 import { applyCuratedDestinationBatch3, applyCuratedDestinationsBatch3 } from "./destination-curation-batch3";
+import { applyCuratedDestinationBatch4, applyCuratedDestinationsBatch4 } from "./destination-curation-batch4";
 import type { Destination } from "./types";
 
 export function applyAllCuratedDestination(destination: Destination): Destination {
-  return applyCuratedDestinationBatch3(
-    applyCuratedDestinationBatch2(applyCuratedDestination(destination)),
+  return applyCuratedDestinationBatch4(
+    applyCuratedDestinationBatch3(
+      applyCuratedDestinationBatch2(applyCuratedDestination(destination)),
+    ),
   );
 }
 
 export function applyAllCuratedDestinations(destinations: Destination[]): Destination[] {
-  return applyCuratedDestinationsBatch3(
-    applyCuratedDestinationsBatch2(applyCuratedDestinations(destinations)),
+  return applyCuratedDestinationsBatch4(
+    applyCuratedDestinationsBatch3(
+      applyCuratedDestinationsBatch2(applyCuratedDestinations(destinations)),
+    ),
   );
 }
