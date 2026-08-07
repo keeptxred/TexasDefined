cc2c65edf0000eb411fd2edac1a91a1b48e2ec0c
keeptxred <freddy.coppola@gmail.com>
2026-08-07 17:39:04 -0500

    Compose third destination curation batch

--- src/data/destination-curation-all.ts
+++ src/data/destination-curation-all.ts
@@ -1,11 +1,16 @@
 import { applyCuratedDestination, applyCuratedDestinations } from "./destination-curation";
 import { applyCuratedDestinationBatch2, applyCuratedDestinationsBatch2 } from "./destination-curation-batch2";
+import { applyCuratedDestinationBatch3, applyCuratedDestinationsBatch3 } from "./destination-curation-batch3";
 import type { Destination } from "./types";
 
 export function applyAllCuratedDestination(destination: Destination): Destination {
-  return applyCuratedDestinationBatch2(applyCuratedDestination(destination));
+  return applyCuratedDestinationBatch3(
+    applyCuratedDestinationBatch2(applyCuratedDestination(destination)),
+  );
 }
 
 export function applyAllCuratedDestinations(destinations: Destination[]): Destination[] {
-  return applyCuratedDestinationsBatch2(applyCuratedDestinations(destinations));
+  return applyCuratedDestinationsBatch3(
+    applyCuratedDestinationsBatch2(applyCuratedDestinations(destinations)),
+  );
 }
