6fa0246e98a8c8084249f21e5265b354109a4b7e
keeptxred <freddy.coppola@gmail.com>
2026-08-07 08:20:23 -0500

    Register five new evergreen articles

---
+++ src/data/fixtures/newest-evergreen.ts
@@ -0,0 +1,13 @@
+import { prepareTexasHouseFreezeArticle } from "./prepare-texas-house-freeze";
+import { mudsPidsHoasSpecialDistrictsArticle } from "./muds-pids-hoas-special-districts";
+import { texasSchoolDistrictsExplainedArticle } from "./texas-school-districts-explained";
+import { chooseElectricityPlanTexasArticle } from "./choose-electricity-plan-texas";
+import { texasHillCountryWhatMakesItArticle } from "./texas-hill-country-what-makes-it";
+
+export const newestEvergreenArticles = [
+  prepareTexasHouseFreezeArticle,
+  mudsPidsHoasSpecialDistrictsArticle,
+  texasSchoolDistrictsExplainedArticle,
+  chooseElectricityPlanTexasArticle,
+  texasHillCountryWhatMakesItArticle,
+];
