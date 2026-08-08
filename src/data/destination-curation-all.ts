ab8b5fbebc67adcbcc4aa15a36c2e27a425c4336
keeptxred <freddy.coppola@gmail.com>
2026-08-07 19:22:28 -0500

    Wire destination hero correction batch 30

--- src/data/destination-curation-all.ts
+++ src/data/destination-curation-all.ts
@@ -27,6 +27,7 @@ import { applyCuratedDestinationBatch26 } from "./destination-curation-batch26";
 import { applyCuratedDestinationBatch27 } from "./destination-curation-batch27";
 import { applyCuratedDestinationBatch28 } from "./destination-curation-batch28";
 import { applyCuratedDestinationBatch29 } from "./destination-curation-batch29";
+import { applyCuratedDestinationBatch30 } from "./destination-curation-batch30";
 import type { Destination } from "./types";
 
 const CURATION_SLUG_ALIASES: Record<string, string> = {
@@ -104,6 +105,7 @@ const CURATORS: Array<(destination: Destination) => Destination> = [
   applyCuratedDestinationBatch27,
   applyCuratedDestinationBatch28,
   applyCuratedDestinationBatch29,
+  applyCuratedDestinationBatch30,
 ];
 
 function runCurators(destination: Destination): Destination {
