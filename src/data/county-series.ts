9f44f20c731619f5140e290031104d546bc978ee
keeptxred <freddy.coppola@gmail.com>
2026-08-11 14:47:22 -0500

    Register Travis County canonical guide

--- src/data/county-series.ts
+++ src/data/county-series.ts
@@ -25,6 +25,7 @@ export const COUNTY_SERIES_PROFILES: CountySeriesProfile[] = [
   { countySlug: "galveston", articleSlug: "galveston-county-island-port-juneteenth-texas", loadArticle: () => import("@/data/fixtures/galveston-county-island-port-juneteenth").then((module) => module.galvestonCountyIslandPortJuneteenthArticle) },
   { countySlug: "gillespie", articleSlug: "gillespie-county-fredericksburg-stonewall-hill-country-texas", loadArticle: () => import("@/data/fixtures/gillespie-county-fredericksburg-stonewall-hill-country").then((module) => module.gillespieCountyFredericksburgStonewallHillCountryArticle) },
   { countySlug: "bexar", articleSlug: "bexar-county-san-antonio-missions-river-texas", loadArticle: () => import("@/data/fixtures/bexar-county-san-antonio-missions-river").then((module) => module.bexarCountySanAntonioMissionsRiverArticle) },
+  { countySlug: "travis", articleSlug: "travis-county-austin-capitol-springs-hill-country-texas", loadArticle: () => import("@/data/fixtures/travis-county-austin-capitol-springs-hill-country").then((module) => module.travisCountyAustinCapitolSpringsHillCountryArticle) },
 ];
 
 const byCountySlug = new Map(COUNTY_SERIES_PROFILES.map((profile) => [profile.countySlug, profile]));
