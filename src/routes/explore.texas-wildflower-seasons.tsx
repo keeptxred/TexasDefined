43cfc4dc2009ee599ab9d9ca22e1a901f42187b4
keeptxred <freddy.coppola@gmail.com>
2026-08-13 20:47:34 -0500

    Point wildflower season alias to canonical guide

--- src/routes/explore.texas-wildflower-seasons.tsx
+++ src/routes/explore.texas-wildflower-seasons.tsx
@@ -2,6 +2,6 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/explore/texas-wildflower-seasons")({
   beforeLoad: ({ location }) => {
-    throw redirect({ href: `/explore/road-trips${location.searchStr || ""}`, statusCode: 301 });
+    throw redirect({ href: `/article/texas-wildflowers-guide${location.searchStr || ""}`, statusCode: 301 });
   },
 });
