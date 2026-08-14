aa6f0630d5125031d15ec74320703c0d84f52dc5
keeptxred <freddy.coppola@gmail.com>
2026-08-14 09:40:58 -0500

    Add fishing access and services sitemap directories

--- src/data/fishing/sitemap.ts
+++ src/data/fishing/sitemap.ts
@@ -1,5 +1,6 @@
 import { FISHING_GUIDES_DIRECTORY_PATH, FISHING_GUIDES_VERIFIED_AT } from "./guide-routing";
 import { LAKE_CONROE_SECTION_SLUGS, LAKE_CONROE_VERIFIED_AT, lakeConroeCanonicalPath } from "./lake-conroe-routing";
+import { FISHING_ACCESS_DIRECTORY_PATH, FISHING_LOCAL_VERIFIED_AT, FISHING_SERVICES_DIRECTORY_PATH } from "./local-routing";
 import { FISHING_REPORTS_DIRECTORY_PATH, FISHING_REPORTS_VERIFIED_AT } from "./report-routing";
 import { SHOWCASE_LAKE_SECTION_SLUGS, SHOWCASE_LAKE_SLUGS, SHOWCASE_LAKE_VERIFIED_AT, showcaseLakeCanonicalPath } from "./showcase-lake-routing";
 import { FISHING_SPECIES_DIRECTORY_PATH, FISHING_SPECIES_VERIFIED_AT, fishingSpeciesCanonicalPath } from "./species-routing";
@@ -11,6 +12,8 @@ export const FISHING_SITEMAP_ENTRIES = [
   { path: FISHING_LAKES_DIRECTORY_PATH, lastmod: FISHING_LAKES_DIRECTORY_VERIFIED_AT },
   { path: FISHING_GUIDES_DIRECTORY_PATH, lastmod: FISHING_GUIDES_VERIFIED_AT },
   { path: FISHING_REPORTS_DIRECTORY_PATH, lastmod: FISHING_REPORTS_VERIFIED_AT },
+  { path: FISHING_ACCESS_DIRECTORY_PATH, lastmod: FISHING_LOCAL_VERIFIED_AT },
+  { path: FISHING_SERVICES_DIRECTORY_PATH, lastmod: FISHING_LOCAL_VERIFIED_AT },
   { path: lakeConroeCanonicalPath(), lastmod: LAKE_CONROE_VERIFIED_AT },
   ...LAKE_CONROE_SECTION_SLUGS.map((section) => ({ path: lakeConroeCanonicalPath(section), lastmod: LAKE_CONROE_VERIFIED_AT })),
   ...SHOWCASE_LAKE_SLUGS.flatMap((slug) => [
