546bf530e67a8c172ebf73c24cf2416b28078e64
keeptxred <freddy.coppola@gmail.com>
2026-08-14 08:06:21 -0500

    Add fishing lakes directory to fishing sitemap

--- src/data/fishing/sitemap.ts
+++ src/data/fishing/sitemap.ts
@@ -2,7 +2,11 @@ import { LAKE_CONROE_SECTION_SLUGS, LAKE_CONROE_VERIFIED_AT, lakeConroeCanonical
 import { SHOWCASE_LAKE_SECTION_SLUGS, SHOWCASE_LAKE_SLUGS, SHOWCASE_LAKE_VERIFIED_AT, showcaseLakeCanonicalPath } from "./showcase-lake-routing";
 import { FISHING_SPECIES_DIRECTORY_PATH, FISHING_SPECIES_VERIFIED_AT, fishingSpeciesCanonicalPath } from "./species-routing";
 
+export const FISHING_LAKES_DIRECTORY_PATH = "/fishing/lakes";
+export const FISHING_LAKES_DIRECTORY_VERIFIED_AT = SHOWCASE_LAKE_VERIFIED_AT;
+
 export const FISHING_SITEMAP_ENTRIES = [
+  { path: FISHING_LAKES_DIRECTORY_PATH, lastmod: FISHING_LAKES_DIRECTORY_VERIFIED_AT },
   { path: lakeConroeCanonicalPath(), lastmod: LAKE_CONROE_VERIFIED_AT },
   ...LAKE_CONROE_SECTION_SLUGS.map((section) => ({ path: lakeConroeCanonicalPath(section), lastmod: LAKE_CONROE_VERIFIED_AT })),
   ...SHOWCASE_LAKE_SLUGS.flatMap((slug) => [
