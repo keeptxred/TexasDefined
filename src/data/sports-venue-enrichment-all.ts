101933608007e0252b3a92a225e3d6b33a0b819b
keeptxred <freddy.coppola@gmail.com>
2026-08-13 22:05:32 -0500

    Combine third sports venue enrichment batch

--- src/data/sports-venue-enrichment-all.ts
+++ src/data/sports-venue-enrichment-all.ts
@@ -1,8 +1,11 @@
 import { getSportsVenueEnrichment, sportsVenueMapUrl } from './sports-venue-enrichment';
 import { getSportsVenueEnrichmentBatch2 } from './sports-venue-enrichment-batch2';
+import { getSportsVenueEnrichmentBatch3 } from './sports-venue-enrichment-batch3';
 
 export { sportsVenueMapUrl };
 
 export function getSportsVenueEnrichmentAll(slug: string) {
-  return getSportsVenueEnrichment(slug) ?? getSportsVenueEnrichmentBatch2(slug);
+  return getSportsVenueEnrichment(slug)
+    ?? getSportsVenueEnrichmentBatch2(slug)
+    ?? getSportsVenueEnrichmentBatch3(slug);
 }
