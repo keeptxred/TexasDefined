62fc7cb2245a7a960ea708fb6019e134bb317d03
keeptxred <freddy.coppola@gmail.com>
2026-08-15 14:58:19 -0500

    Publish complete species guides in fishing sitemap

--- src/data/fishing/sitemap.ts
+++ src/data/fishing/sitemap.ts
@@ -6,6 +6,7 @@ import { FISHING_REGULATIONS_PATH, FISHING_REGULATIONS_VERIFIED_AT } from "./reg
 import { FISHING_REPORTS_DIRECTORY_PATH, FISHING_REPORTS_VERIFIED_AT } from "./report-routing";
 import { FISHING_SEASONS_PATH, FISHING_SEASONS_VERIFIED_AT } from "./season-routing";
 import { SHOWCASE_LAKE_SECTION_SLUGS, SHOWCASE_LAKE_SLUGS, SHOWCASE_LAKE_VERIFIED_AT, showcaseLakeCanonicalPath } from "./showcase-lake-routing";
+import { COMPLETE_FISHING_SPECIES_SLUGS } from "./slugs";
 import { FISHING_SPECIES_DIRECTORY_PATH, FISHING_SPECIES_VERIFIED_AT, fishingSpeciesCanonicalPath } from "./species-routing";
 import { FISHING_TECHNIQUES_DIRECTORY_PATH, FISHING_TECHNIQUES_VERIFIED_AT, PUBLISHED_FISHING_TECHNIQUE_SLUGS, fishingTechniqueCanonicalPath } from "./technique-routing";
 
@@ -31,5 +32,5 @@ export const FISHING_SITEMAP_ENTRIES = [
     ...SHOWCASE_LAKE_SECTION_SLUGS.map((section) => ({ path: showcaseLakeCanonicalPath(slug, section), lastmod: SHOWCASE_LAKE_VERIFIED_AT })),
   ]),
   { path: FISHING_SPECIES_DIRECTORY_PATH, lastmod: FISHING_SPECIES_VERIFIED_AT },
-  { path: fishingSpeciesCanonicalPath("largemouth-bass"), lastmod: FISHING_SPECIES_VERIFIED_AT },
+  ...COMPLETE_FISHING_SPECIES_SLUGS.map((slug) => ({ path: fishingSpeciesCanonicalPath(slug), lastmod: FISHING_SPECIES_VERIFIED_AT })),
 ] as const;
