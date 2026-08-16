9015d23bd54645711400d71efee69e47cc6193e2
keeptxred <freddy.coppola@gmail.com>
2026-08-16 14:25:39 -0500

    Record upstream office freshness in property snapshot

--- src/data/property/county-property-enrichment.generated.ts
+++ src/data/property/county-property-enrichment.generated.ts
@@ -4,6 +4,7 @@ export type CountyPropertyEnrichment = {
   appraisalDistrict: Partial<CountyOfficeContact>;
   taxOffice: Partial<CountyOfficeContact>;
   links: Partial<CountyPropertyLinks>;
+  sourceUpdatedAt: { appraisalDistrict: string; taxOffice: string };
   lastVerifiedAt: string;
   sourceUrls: string[];
 };
@@ -19,6 +20,7 @@ export const COUNTY_PROPERTY_ENRICHMENT: Record<string, CountyPropertyEnrichment
     appraisalDistrict: { name: 'Tim Chambers', websiteUrl: 'https://www.angelinacad.org', phone: '936-634-8456', address: '105 Miles Way, Ste. 300, Lufkin, TX 75901-5980', email: 'cdowns@angelinacad.org' },
     taxOffice: { name: 'Terri Collier', websiteUrl: 'https://www.angelinacounty.net', phone: '936-634-8376', address: '211 E. Shepherd Ave., Lufkin, TX 75901', email: 'taxoffice@angelinacounty.net' },
     links: { appraisalDistrictUrl: 'https://www.angelinacad.org', taxOfficeUrl: 'https://www.angelinacounty.net' },
+    sourceUpdatedAt: { appraisalDistrict: '2026-05-05', taxOffice: '2025-03-03' },
     lastVerifiedAt: '2026-08-16',
     sourceUrls: ['https://comptroller.texas.gov/taxes/property-tax/county-directory/angelina.php', 'https://www.angelinacad.org', 'https://www.angelinacounty.net'],
   },
@@ -26,6 +28,7 @@ export const COUNTY_PROPERTY_ENRICHMENT: Record<string, CountyPropertyEnrichment
     appraisalDistrict: { name: 'Deb Castaldo', websiteUrl: 'https://www.beecad.org', phone: '361-358-0193', address: '401 N. Washington St., Beeville, TX 78102-3911', email: 'bee@beecad.org' },
     taxOffice: { name: 'Michelle Matus', websiteUrl: 'https://www.co.bee.tx.us', phone: '361-621-1554', address: '411 E. Houston St., Beeville, TX 78102-4938', email: 'michelle.matus@beecounty.texas.gov' },
     links: { appraisalDistrictUrl: 'https://www.beecad.org', taxOfficeUrl: 'https://www.co.bee.tx.us' },
+    sourceUpdatedAt: { appraisalDistrict: '2026-05-05', taxOffice: '2025-03-03' },
     lastVerifiedAt: '2026-08-16',
     sourceUrls: ['https://comptroller.texas.gov/taxes/property-tax/county-directory/bee.php', 'https://www.beecad.org', 'https://www.co.bee.tx.us'],
   },
@@ -33,6 +36,7 @@ export const COUNTY_PROPERTY_ENRICHMENT: Record<string, CountyPropertyEnrichment
     appraisalDistrict: { name: 'Brittany Jameson', websiteUrl: 'https://www.collingsworthcad.org', phone: '806-447-5172', address: '800 West Ave., Rm. 1, Wellington, TX 79095-3037', email: 'bjameson@collingsworthcad.org' },
     taxOffice: { name: 'Sharon Sherwood', websiteUrl: 'https://www.co.collingsworth.tx.us', phone: '806-447-5606', address: '800 West Ave., Box 2, Wellington, TX 79095-3037', email: 'sharon.chism@co.collingsworth.tx.us' },
     links: { appraisalDistrictUrl: 'https://www.collingsworthcad.org', taxOfficeUrl: 'https://www.co.collingsworth.tx.us' },
+    sourceUpdatedAt: { appraisalDistrict: '2026-05-05', taxOffice: '2025-03-13' },
     lastVerifiedAt: '2026-08-16',
     sourceUrls: ['https://comptroller.texas.gov/taxes/property-tax/county-directory/collingsworth.php', 'https://www.collingsworthcad.org', 'https://www.co.collingsworth.tx.us'],
   },
@@ -40,6 +44,7 @@ export const COUNTY_PROPERTY_ENRICHMENT: Record<string, CountyPropertyEnrichment
     appraisalDistrict: { name: 'Gary Zeitler, Interim', websiteUrl: 'https://www.fishercad.org', phone: '325-776-2733', address: '107 E. North 1st St., Roby, TX 79543-2301', email: 'hbufkin@fishercad.org' },
     taxOffice: { name: 'Jonnye Lu Speck', websiteUrl: 'https://www.fishercounty.org', phone: '325-776-2181', address: '100 N. Concho St., Roby, TX 79543-2344', email: 'jonnye.speck@fishercounty.org' },
     links: { appraisalDistrictUrl: 'https://www.fishercad.org', taxOfficeUrl: 'https://www.fishercounty.org' },
+    sourceUpdatedAt: { appraisalDistrict: '2025-04-09', taxOffice: '2025-03-25' },
     lastVerifiedAt: '2026-08-16',
     sourceUrls: ['https://comptroller.texas.gov/taxes/property-tax/county-directory/fisher.php', 'https://www.fishercad.org', 'https://www.fishercounty.org'],
   },
@@ -47,6 +52,7 @@ export const COUNTY_PROPERTY_ENRICHMENT: Record<string, CountyPropertyEnrichment
     appraisalDistrict: { name: 'Laura Raven', websiteUrl: 'https://hayscad.com', phone: '512-268-2522', address: '21001 N. IH 35, Kyle, TX 78640-4795', email: 'info@hayscad.com' },
     taxOffice: { name: 'Jennifer Escobar', websiteUrl: 'https://www.hayscountytx.gov', phone: '512-393-5545', address: '712 S. Stagecoach Trail, San Marcos, TX 78666-6073', email: 'propertytax@hayscountytx.gov' },
     links: { appraisalDistrictUrl: 'https://hayscad.com', taxOfficeUrl: 'https://www.hayscountytx.gov' },
+    sourceUpdatedAt: { appraisalDistrict: '2026-07-02', taxOffice: '2025-12-04' },
     lastVerifiedAt: '2026-08-16',
     sourceUrls: ['https://comptroller.texas.gov/taxes/property-tax/county-directory/hays.php', 'https://hayscad.com', 'https://www.hayscountytx.gov'],
   },
@@ -54,6 +60,7 @@ export const COUNTY_PROPERTY_ENRICHMENT: Record<string, CountyPropertyEnrichment
     appraisalDistrict: { name: 'Rolando Garza', websiteUrl: 'https://www.hidalgoad.org', phone: '956-381-8466', address: '4405 S. Professional Dr., Edinburg, TX 78539-6556', email: 'cs@hidalgoad.org' },
     taxOffice: { name: 'Paul Villarreal Jr.', websiteUrl: 'https://www.hidalgocounty.us', phone: '956-318-2157', address: '2804 S. US Hwy. 281, Edinburg, TX 78539-6243', email: 'paul.villarreal@hidalgocountytax.org' },
     links: { appraisalDistrictUrl: 'https://www.hidalgoad.org', taxOfficeUrl: 'https://www.hidalgocounty.us' },
+    sourceUpdatedAt: { appraisalDistrict: '2025-06-11', taxOffice: '2025-03-26' },
     lastVerifiedAt: '2026-08-16',
     sourceUrls: ['https://comptroller.texas.gov/taxes/property-tax/county-directory/hidalgo.php', 'https://www.hidalgoad.org', 'https://www.hidalgocounty.us'],
   },
@@ -61,6 +68,7 @@ export const COUNTY_PROPERTY_ENRICHMENT: Record<string, CountyPropertyEnrichment
     appraisalDistrict: { name: 'Marcus Williams', websiteUrl: 'https://www.leoncad.org', phone: '903-536-2252', address: '141 W. Saint Marys St., Centerville, TX 75833-3456', email: 'leoncentralappraisal@gmail.com' },
     taxOffice: { name: 'Victoria Willis', websiteUrl: 'https://www.co.leon.tx.us', phone: '903-536-2543', address: '155 N. Cass St., Centerville, TX 75833', email: 'victoria.willis@co.leon.tx.us' },
     links: { appraisalDistrictUrl: 'https://www.leoncad.org', taxOfficeUrl: 'https://www.co.leon.tx.us' },
+    sourceUpdatedAt: { appraisalDistrict: '2025-08-13', taxOffice: '2025-04-16' },
     lastVerifiedAt: '2026-08-16',
     sourceUrls: ['https://comptroller.texas.gov/taxes/property-tax/county-directory/leon.php', 'https://www.leoncad.org', 'https://www.co.leon.tx.us'],
   },
@@ -68,6 +76,7 @@ export const COUNTY_PROPERTY_ENRICHMENT: Record<string, CountyPropertyEnrichment
     appraisalDistrict: { name: 'Tim Radloff', websiteUrl: 'https://www.lubbockcad.org', phone: '806-762-5000', address: '2109 Avenue Q, Lubbock, TX 79411-1524', email: 'info@lubbockcad.org' },
     taxOffice: { name: 'Ronnie Keister', websiteUrl: 'https://www.lubbockcounty.gov', phone: '806-775-1344', address: '916 Main St., Suite 102, Lubbock, TX 79401-3412', email: 'taxoffice@lubbockcounty.gov' },
     links: { appraisalDistrictUrl: 'https://www.lubbockcad.org', taxOfficeUrl: 'https://www.lubbockcounty.gov' },
+    sourceUpdatedAt: { appraisalDistrict: '2026-02-25', taxOffice: '2025-04-22' },
     lastVerifiedAt: '2026-08-16',
     sourceUrls: ['https://comptroller.texas.gov/taxes/property-tax/county-directory/lubbock.php', 'https://www.lubbockcad.org', 'https://www.lubbockcounty.gov'],
   },
@@ -75,6 +84,7 @@ export const COUNTY_PROPERTY_ENRICHMENT: Record<string, CountyPropertyEnrichment
     appraisalDistrict: { name: 'Tina Ford', websiteUrl: 'https://www.southwestdatasolution.com/webindex.aspx?dbkey=SABINECAD', phone: '409-787-2777', address: '1920 Worth St., Hemphill, TX 75948-9998', email: 'sabinecad@windstream.net' },
     taxOffice: { name: 'Martha M. Stone', websiteUrl: 'https://www.co.sabine.tx.us', phone: '409-787-2257', address: '213 Market St., Hemphill, TX 75948', email: 'martha.stone@co.sabine.tx.us' },
     links: { appraisalDistrictUrl: 'https://www.southwestdatasolution.com/webindex.aspx?dbkey=SABINECAD', taxOfficeUrl: 'https://www.co.sabine.tx.us' },
+    sourceUpdatedAt: { appraisalDistrict: '2026-05-06', taxOffice: '2025-02-19' },
     lastVerifiedAt: '2026-08-16',
     sourceUrls: ['https://comptroller.texas.gov/taxes/property-tax/county-directory/sabine.php', 'https://www.southwestdatasolution.com/webindex.aspx?dbkey=SABINECAD', 'https://www.co.sabine.tx.us'],
   },
@@ -82,6 +92,7 @@ export const COUNTY_PROPERTY_ENRICHMENT: Record<string, CountyPropertyEnrichment
     appraisalDistrict: { name: 'Carol McNeil', websiteUrl: 'https://www.smithcad.org', phone: '903-510-8600', address: '245 S. S.E. Loop 323, Tyler, TX 75702-6456', email: 'chiefappraiser@scad.org' },
     taxOffice: { name: 'Gary B. Barber', websiteUrl: 'https://www.smith-county.com', phone: '903-590-2920', address: '1517 W. Front St., Tyler, TX 75702-7822', email: 'taxoffice@smith-county.com' },
     links: { appraisalDistrictUrl: 'https://www.smithcad.org', taxOfficeUrl: 'https://www.smith-county.com' },
+    sourceUpdatedAt: { appraisalDistrict: '2025-01-29', taxOffice: '2025-04-25' },
     lastVerifiedAt: '2026-08-16',
     sourceUrls: ['https://comptroller.texas.gov/taxes/property-tax/county-directory/smith.php', 'https://www.smithcad.org', 'https://www.smith-county.com'],
   },
@@ -89,6 +100,7 @@ export const COUNTY_PROPERTY_ENRICHMENT: Record<string, CountyPropertyEnrichment
     appraisalDistrict: { name: 'Blain Chriesman', websiteUrl: 'https://www.terrellcad.org', phone: '432-345-2251', address: '302 N. 2nd St., Sanderson, TX 79848', email: 'tcad@terrell.esc18.net' },
     taxOffice: { name: 'Thad Cleveland', websiteUrl: 'https://www.co.terrell.tx.us', phone: '432-345-2499', address: '105 E. Hackberry St., Sanderson, TX 79848', email: 'thad.cleveland@co.terrell.tx.us' },
     links: { appraisalDistrictUrl: 'https://www.terrellcad.org', taxOfficeUrl: 'https://www.co.terrell.tx.us' },
+    sourceUpdatedAt: { appraisalDistrict: '2025-01-30', taxOffice: '2025-04-28' },
     lastVerifiedAt: '2026-08-16',
     sourceUrls: ['https://comptroller.texas.gov/taxes/property-tax/county-directory/terrell.php', 'https://www.terrellcad.org', 'https://www.co.terrell.tx.us'],
   },
@@ -96,6 +108,7 @@ export const COUNTY_PROPERTY_ENRICHMENT: Record<string, CountyPropertyEnrichment
     appraisalDistrict: { name: 'Dyann White', websiteUrl: 'https://www.washingtoncad.org', phone: '979-277-3740', address: '1301 Niebuhr St., Brenham, TX 77833-5031', email: 'wcad@brenhamk-12.net' },
     taxOffice: { name: 'Cheryl Gaskamp', websiteUrl: 'https://www.co.washington.tx.us', phone: '979-277-6200', address: '100 E. Main St., Suite 100, Brenham, TX 77833-3701', email: 'cgaskamp@washingtoncountytx.gov' },
     links: { appraisalDistrictUrl: 'https://www.washingtoncad.org', taxOfficeUrl: 'https://www.co.washington.tx.us' },
+    sourceUpdatedAt: { appraisalDistrict: '2025-02-19', taxOffice: '2025-02-19' },
     lastVerifiedAt: '2026-08-16',
     sourceUrls: ['https://comptroller.texas.gov/taxes/property-tax/county-directory/washington.php', 'https://www.washingtoncad.org', 'https://www.co.washington.tx.us'],
   },
