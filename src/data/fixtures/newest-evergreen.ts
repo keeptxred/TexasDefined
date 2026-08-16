bf7c0892a0fbf1d6628184cbea3aefb82654ff0e
keeptxred <freddy.coppola@gmail.com>
2026-08-16 10:50:56 -0500

    Register Texas Explained support articles

--- src/data/fixtures/newest-evergreen.ts
+++ src/data/fixtures/newest-evergreen.ts
@@ -1,5 +1,7 @@
+import { texasExplainedSupportArticles } from "./texas-explained-support-articles";
 import { winklerCountyKermitWinkOilArticle } from "./winkler-county-kermit-wink-oil";
 
 export const newestEvergreenArticles = [
+  ...texasExplainedSupportArticles,
   winklerCountyKermitWinkOilArticle,
 ];
