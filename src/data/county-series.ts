5b0a368fa727278934f91e27c4d3c4a341af73cb
keeptxred <freddy.coppola@gmail.com>
2026-08-18 04:35:29 -0500

    Map Zapata County legacy article redirect

--- src/data/county-series.ts
+++ src/data/county-series.ts
@@ -64,6 +64,7 @@ const LEGACY_ARTICLE_TO_COUNTY = new Map<string, string>([
   ["cameron-county-brownsville-harlingen-south-padre-rio-grande-texas", "cameron"],
   ["hidalgo-county-edinburg-mcallen-mission-rio-grande-valley-texas", "hidalgo"],
   ["starr-county-rio-grande-city-roma-fort-ringgold-borderlands-texas", "starr"],
+  ["zapata-county-zapata-san-ygnacio-falcon-rio-grande-texas", "zapata"],
 ]);
 
 const COUNTY_SLUGS = new Set(LEGACY_ARTICLE_TO_COUNTY.values());
