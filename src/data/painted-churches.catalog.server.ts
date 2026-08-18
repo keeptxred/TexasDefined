22c8999956b2dce98d3f447f7eef39d59900739f
keeptxred <freddy.coppola@gmail.com>
2026-08-18 07:40:40 -0500

    Add server-only painted church catalog facade

---
+++ src/data/painted-churches.catalog.server.ts
@@ -0,0 +1,2 @@
+export { paintedChurches, schulenburgCoreRoute, getPaintedChurch, PAINTED_CHURCHES_KEY_SOURCES } from "./painted-churches";
+export type { PaintedChurch, PaintedChurchImage } from "./painted-churches";
