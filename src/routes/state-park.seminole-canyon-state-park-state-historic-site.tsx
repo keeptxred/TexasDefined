7c0a6e2b8aeaefdc3bd22971da4f531f2c935e94
keeptxred <freddy.coppola@gmail.com>
2026-08-19 17:35:46 -0500

    Redirect Seminole Canyon state-park mirror

---
+++ src/routes/state-park.seminole-canyon-state-park-state-historic-site.tsx
@@ -0,0 +1,2 @@
+import { createFileRoute, redirect } from "@tanstack/react-router";
+export const Route = createFileRoute("/state-park/seminole-canyon-state-park-state-historic-site")({ beforeLoad: ({ location }) => { throw redirect({ href: `/destination/seminole-canyon-state-park-and-historic-site${location.searchStr || ""}`, statusCode: 301 }); } });
