990570f362e396719ffecba60853ab0e37925fcc
keeptxred <freddy.coppola@gmail.com>
2026-08-19 17:28:38 -0500

    Redirect Devils River Del Norte attraction mirror

---
+++ src/routes/attraction.devils-river-del-norte-unit-state-natural-area.tsx
@@ -0,0 +1,7 @@
+import { createFileRoute, redirect } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/attraction/devils-river-del-norte-unit-state-natural-area")({
+  beforeLoad: ({ location }) => {
+    throw redirect({ href: `/destination/devils-river-state-natural-area${location.searchStr || ""}`, statusCode: 301 });
+  },
+});
