89adf62b26199dfe1f6c146aee2ff7350611e7ca
keeptxred <freddy.coppola@gmail.com>
2026-08-19 15:24:42 -0500

    Add breakfast taco hero to social metadata

--- src/routes/texas-breakfast-taco-guide.tsx
+++ src/routes/texas-breakfast-taco-guide.tsx
@@ -7,6 +7,7 @@ import { buildMeta, canonicalLink } from "@/lib/seo";
 
 const guide = getTexasEvergreenGuideBatch4("texas-breakfast-taco-guide");
 const canonicalPath = "/texas-breakfast-taco-guide";
+const heroImage = "https://commons.wikimedia.org/wiki/Special:Redirect/file/BreakfastTaco.jpg?width=1200";
 
 export const Route = createFileRoute(canonicalPath)({
   head: () => ({
@@ -15,6 +16,8 @@ export const Route = createFileRoute(canonicalPath)({
       title: "Texas Breakfast Tacos: Fillings, Regions & How to Order",
       description: guide.dek,
       type: "article",
+      image: heroImage,
+      imageAlt: "Egg and sausage breakfast taco with salsa on a flour tortilla",
     }),
     links: [canonicalLink(texasDefinedBrand, canonicalPath)],
   }),
