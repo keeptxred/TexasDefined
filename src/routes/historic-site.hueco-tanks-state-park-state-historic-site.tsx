b082622342c322b1b33505f520d0a7b75fd93f4d
keeptxred <freddy.coppola@gmail.com>
2026-08-19 17:27:00 -0500

    Redirect Hueco Tanks historic-site mirror

---
+++ src/routes/historic-site.hueco-tanks-state-park-state-historic-site.tsx
@@ -0,0 +1,7 @@
+import { createFileRoute, redirect } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/historic-site/hueco-tanks-state-park-state-historic-site")({
+  beforeLoad: ({ location }) => {
+    throw redirect({ href: `/destination/hueco-tanks-state-park-and-historic-site${location.searchStr || ""}`, statusCode: 301 });
+  },
+});
