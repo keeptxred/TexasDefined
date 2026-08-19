b7303430aec4108c62ed460d46f9c23d56d385cb
keeptxred <freddy.coppola@gmail.com>
2026-08-19 03:32:36 -0500

    Register Mills County editorial profile

--- src/data/county-series-profiles.ts
+++ src/data/county-series-profiles.ts
@@ -98,4 +98,5 @@ export const COUNTY_SERIES_PROFILES: CountySeriesProfile[] = [
   profile("mason", "mason-county-mason-fort-llano-river-hill-country-texas", () => import("@/data/fixtures/mason-county-mason-fort-llano-river-hill-country").then((module) => module.masonCountyMasonFortLlanoRiverHillCountryArticle)),
   profile("san-saba", "san-saba-county-san-saba-pecans-rivers-hill-country-texas", () => import("@/data/fixtures/san-saba-county-san-saba-pecans-rivers-hill-country").then((module) => module.sanSabaCountySanSabaPecansRiversHillCountryArticle)),
   profile("lampasas", "lampasas-county-lampasas-springs-rivers-central-texas", () => import("@/data/fixtures/lampasas-county-lampasas-springs-rivers-central-texas").then((module) => module.lampasasCountyLampasasSpringsRiversCentralTexasArticle)),
+  profile("mills", "mills-county-goldthwaite-colorado-river-ranch-country-texas", () => import("@/data/fixtures/mills-county-goldthwaite-colorado-river-ranch-country").then((module) => module.millsCountyGoldthwaiteColoradoRiverRanchCountryArticle)),
 ];
