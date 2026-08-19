e5d4570689cae12602dc1dc4c6deaadeb02bffc8
keeptxred <freddy.coppola@gmail.com>
2026-08-19 16:50:28 -0500

    Add puffy taco social hero metadata

--- src/routes/san-antonio-puffy-taco-history.tsx
+++ src/routes/san-antonio-puffy-taco-history.tsx
@@ -7,6 +7,7 @@ import { buildMeta, canonicalLink } from "@/lib/seo";
 
 const guide = getTexasEvergreenGuideBatch5("san-antonio-puffy-taco-history");
 const canonicalPath = "/san-antonio-puffy-taco-history";
+const heroImage = "https://commons.wikimedia.org/wiki/Special:Redirect/file/Puffy_taco.jpg?width=1600";
 
 export const Route = createFileRoute(canonicalPath)({
   head: () => ({
@@ -15,6 +16,8 @@ export const Route = createFileRoute(canonicalPath)({
       title: "San Antonio Puffy Tacos: History of a Texas City Icon",
       description: guide.dek,
       type: "article",
+      image: heroImage,
+      imageAlt: "Puffy taco served at Los Barrios Mexican Restaurant in San Antonio",
     }),
     links: [canonicalLink(texasDefinedBrand, canonicalPath)],
   }),
