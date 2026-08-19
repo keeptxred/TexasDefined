f1467fdf128715055d6776604695952d5198b390
keeptxred <freddy.coppola@gmail.com>
2026-08-19 13:04:57 -0500

    Add canonical Texas icons reference rows

---
+++ src/data/things-unique-to-texas-reference.ts
@@ -0,0 +1,21 @@
+import { TEXAS_ICON_CATEGORIES } from "./things-unique-to-texas";
+import { texasIconCanonicalHref } from "./things-unique-to-texas-links";
+
+export const TEXAS_ICONS_COLLECTION_URL = "https://texasdefined.com/things-unique-to-texas";
+export const TEXAS_ICONS_METHODOLOGY_URL = "https://texasdefined.com/things-unique-to-texas/methodology";
+
+export const TEXAS_ICON_REFERENCE_ROWS = TEXAS_ICON_CATEGORIES.flatMap((category) =>
+  category.items.map((entry) => {
+    const deeperGuidePath = texasIconCanonicalHref(entry);
+    return {
+      id: entry.id,
+      name: entry.name,
+      description: entry.note,
+      chapter: category.slug,
+      chapterTitle: category.title,
+      canonicalCollection: TEXAS_ICONS_COLLECTION_URL,
+      methodology: TEXAS_ICONS_METHODOLOGY_URL,
+      deeperGuide: deeperGuidePath ? `https://texasdefined.com${deeperGuidePath}` : null,
+    };
+  }),
+);
