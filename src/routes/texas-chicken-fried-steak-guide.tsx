d58cb364d295154dd9dfe04314d16a155b726c5b
keeptxred <freddy.coppola@gmail.com>
2026-08-19 15:24:33 -0500

    Add chicken-fried steak hero to social metadata

--- src/routes/texas-chicken-fried-steak-guide.tsx
+++ src/routes/texas-chicken-fried-steak-guide.tsx
@@ -7,6 +7,7 @@ import { buildMeta, canonicalLink } from "@/lib/seo";
 
 const guide = getTexasEvergreenGuideBatch4("texas-chicken-fried-steak-guide");
 const canonicalPath = "/texas-chicken-fried-steak-guide";
+const heroImage = "https://commons.wikimedia.org/wiki/Special:Redirect/file/Chicken_fried_steak.jpg?width=1600";
 
 export const Route = createFileRoute(canonicalPath)({
   head: () => ({
@@ -15,6 +16,8 @@ export const Route = createFileRoute(canonicalPath)({
       title: "Texas Chicken-Fried Steak: History, Styles & Cream Gravy",
       description: guide.dek,
       type: "article",
+      image: heroImage,
+      imageAlt: "Chicken-fried steak served as a classic breaded beef comfort-food plate",
     }),
     links: [canonicalLink(texasDefinedBrand, canonicalPath)],
   }),
