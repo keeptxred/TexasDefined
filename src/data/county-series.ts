6fb2c9eaee0dc885864dba24fbcbc21cba34203f
keeptxred <freddy.coppola@gmail.com>
2026-08-19 06:33:03 -0500

    Add Eastland County legacy redirect

--- src/data/county-series.ts
+++ src/data/county-series.ts
@@ -105,6 +105,7 @@ const LEGACY_ARTICLE_TO_COUNTY = new Map<string, string>([
   ["bosque-county-meridian-clifton-norwegian-heritage-bosque-river-texas", "bosque"],
   ["erath-county-stephenville-dublin-tarleton-cross-timbers-texas", "erath"],
   ["comanche-county-comanche-de-leon-proctor-lake-cross-timbers-texas", "comanche"],
+  ["eastland-county-eastland-cisco-ranger-oil-cross-timbers-texas", "eastland"],
 ]);
 
 const COUNTY_SLUGS = new Set(LEGACY_ARTICLE_TO_COUNTY.values());
