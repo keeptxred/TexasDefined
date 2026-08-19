78f1dfa02f1ce172a153976692b046a72d942fc6
keeptxred <freddy.coppola@gmail.com>
2026-08-19 15:24:52 -0500

    Add Dr Pepper hero to social metadata

--- src/routes/dr-pepper-texas-history.tsx
+++ src/routes/dr-pepper-texas-history.tsx
@@ -7,6 +7,7 @@ import { buildMeta, canonicalLink } from "@/lib/seo";
 
 const guide = getTexasEvergreenGuideBatch4("dr-pepper-texas-history");
 const canonicalPath = "/dr-pepper-texas-history";
+const heroImage = "https://commons.wikimedia.org/wiki/Special:Redirect/file/Cupola_Dr_Pepper_Museum_Waco_Texas_2024.jpg?width=1600";
 
 export const Route = createFileRoute(canonicalPath)({
   head: () => ({
@@ -15,6 +16,8 @@ export const Route = createFileRoute(canonicalPath)({
       title: "Dr Pepper in Texas: How a Waco Soda Became a State Icon",
       description: guide.dek,
       type: "article",
+      image: heroImage,
+      imageAlt: "Cupola and upper exterior of the Dr Pepper Museum in Waco, Texas",
     }),
     links: [canonicalLink(texasDefinedBrand, canonicalPath)],
   }),
