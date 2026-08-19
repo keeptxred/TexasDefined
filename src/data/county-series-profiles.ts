77c539bb8bdb87e495e0eac4f7c56f05a3c6ea46
keeptxred <freddy.coppola@gmail.com>
2026-08-19 07:40:30 -0500

    Register Hood County editorial profile

--- src/data/county-series-profiles.ts
+++ src/data/county-series-profiles.ts
@@ -106,4 +106,5 @@ export const COUNTY_SERIES_PROFILES: CountySeriesProfile[] = [
   profile("comanche", "comanche-county-comanche-de-leon-proctor-lake-cross-timbers-texas", () => import("@/data/fixtures/comanche-county-comanche-de-leon-proctor-lake-cross-timbers").then((module) => module.comancheCountyComancheDeLeonProctorLakeCrossTimbersArticle)),
   profile("eastland", "eastland-county-eastland-cisco-ranger-oil-cross-timbers-texas", () => import("@/data/fixtures/eastland-county-eastland-cisco-ranger-oil-cross-timbers").then((module) => module.eastlandCountyEastlandCiscoRangerOilCrossTimbersArticle)),
   profile("palo-pinto", "palo-pinto-county-mineral-wells-possum-kingdom-brazos-cross-timbers-texas", () => import("@/data/fixtures/palo-pinto-county-mineral-wells-possum-kingdom-brazos-cross-timbers").then((module) => module.paloPintoCountyMineralWellsPossumKingdomBrazosCrossTimbersArticle)),
+  profile("hood", "hood-county-granbury-lake-brazos-acton-cross-timbers-texas", () => import("@/data/fixtures/hood-county-granbury-lake-brazos-acton-cross-timbers").then((module) => module.hoodCountyGranburyLakeBrazosActonCrossTimbersArticle)),
 ];
