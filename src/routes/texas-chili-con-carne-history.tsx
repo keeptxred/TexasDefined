b43316f300e4a39d0de297b698649702bb25edec
keeptxred <freddy.coppola@gmail.com>
2026-08-19 15:24:25 -0500

    Add chili hero to social metadata

--- src/routes/texas-chili-con-carne-history.tsx
+++ src/routes/texas-chili-con-carne-history.tsx
@@ -7,6 +7,7 @@ import { buildMeta, canonicalLink } from "@/lib/seo";
 
 const guide = getTexasEvergreenGuideBatch4("texas-chili-con-carne-history");
 const canonicalPath = "/texas-chili-con-carne-history";
+const heroImage = "https://commons.wikimedia.org/wiki/Special:Redirect/file/Pot_of_Chili_Con_Carne.jpg?width=1600";
 
 export const Route = createFileRoute(canonicalPath)({
   head: () => ({
@@ -15,6 +16,8 @@ export const Route = createFileRoute(canonicalPath)({
       title: "Texas Chili Con Carne: Chili Queens, San Antonio & the Bowl of Red",
       description: guide.dek,
       type: "article",
+      image: heroImage,
+      imageAlt: "Pot of chili con carne representing the Texas bowl-of-red tradition",
     }),
     links: [canonicalLink(texasDefinedBrand, canonicalPath)],
   }),
