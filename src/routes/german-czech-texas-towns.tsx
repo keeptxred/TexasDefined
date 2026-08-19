de6b9ddc9571aef59a87a1ffb6ff0fb860cd3086
keeptxred <freddy.coppola@gmail.com>
2026-08-19 13:17:32 -0500

    Add German Czech heritage preview image

--- src/routes/german-czech-texas-towns.tsx
+++ src/routes/german-czech-texas-towns.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import kolacheKlobasnek from "@/assets/kolache-klobasnek-hero-photo.jpg";
 import { texasDefinedBrand } from "@/brand/texasdefined";
 import { TexasEvergreenGuide } from "@/components/editorial/TexasEvergreenGuide";
 import { getTexasEvergreenGuideBatch2 } from "@/data/texas-evergreen-guides-batch2";
@@ -14,6 +15,8 @@ export const Route = createFileRoute(canonicalPath)({
       canonicalPath,
       title: "German & Czech Texas Towns: Food, Churches and Heritage",
       description: guide.dek,
+      image: kolacheKlobasnek,
+      imageAlt: "Texas Czech-style kolache and klobasnek pastries",
       type: "article",
     }),
     links: [canonicalLink(texasDefinedBrand, canonicalPath)],
