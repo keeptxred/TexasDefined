c1626e40f701fa33df1b0721eb436a14f9d4ae53
keeptxred <freddy.coppola@gmail.com>
2026-08-19 17:37:05 -0500

    Redirect Cooper Lake Johns Creek destination alias

---
+++ src/routes/destination.cooper-lake-johns-creek-unit-state-park.tsx
@@ -0,0 +1,2 @@
+import { createFileRoute, redirect } from "@tanstack/react-router";
+export const Route = createFileRoute("/destination/cooper-lake-johns-creek-unit-state-park")({ beforeLoad: ({ location }) => { throw redirect({ href: `/destination/cooper-lake-state-park${location.searchStr || ""}`, statusCode: 301 }); } });
