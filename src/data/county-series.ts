9ff7d81880ef9deb4c8832d6d7755b8962e77c0f
keeptxred <freddy.coppola@gmail.com>
2026-08-19 01:04:50 -0500

    Add Burnet County legacy redirect

--- src/data/county-series.ts
+++ src/data/county-series.ts
@@ -94,6 +94,7 @@ const LEGACY_ARTICLE_TO_COUNTY = new Map<string, string>([
   ["kendall-county-boerne-comfort-guadalupe-hill-country-texas", "kendall"],
   ["kerr-county-kerrville-guadalupe-hunt-hill-country-texas", "kerr"],
   ["blanco-county-johnson-city-blanco-pedernales-hill-country-texas", "blanco"],
+  ["burnet-county-burnet-marble-falls-highland-lakes-granite-texas", "burnet"],
 ]);
 
 const COUNTY_SLUGS = new Set(LEGACY_ARTICLE_TO_COUNTY.values());
