8f3d237f4af083295a2a04fa2fcf358e3070f455
keeptxred <freddy.coppola@gmail.com>
2026-08-19 16:50:21 -0500

    Add Ranch Water social hero metadata

--- src/routes/texas-ranch-water-guide.tsx
+++ src/routes/texas-ranch-water-guide.tsx
@@ -7,6 +7,7 @@ import { buildMeta, canonicalLink } from "@/lib/seo";
 
 const guide = getTexasEvergreenGuideBatch5("texas-ranch-water-guide");
 const canonicalPath = "/texas-ranch-water-guide";
+const heroImage = "https://commons.wikimedia.org/wiki/Special:Redirect/file/Ranch_water.jpg?width=1600";
 
 export const Route = createFileRoute(canonicalPath)({
   head: () => ({
@@ -15,6 +16,8 @@ export const Route = createFileRoute(canonicalPath)({
       title: "Texas Ranch Water: History, Origin Stories & What It Is",
       description: guide.dek,
       type: "article",
+      image: heroImage,
+      imageAlt: "Ranch Water cocktail served over ice with citrus",
     }),
     links: [canonicalLink(texasDefinedBrand, canonicalPath)],
   }),
