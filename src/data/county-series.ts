c054cb40f30bffddcd61843657b0ac44a2a5060d
keeptxred <freddy.coppola@gmail.com>
2026-08-19 17:34:38 -0500

    Map Upshur County legacy slug

--- src/data/county-series.ts
+++ src/data/county-series.ts
@@ -131,6 +131,7 @@ const LEGACY_ARTICLE_TO_COUNTY = new Map<string, string>([
   ["cherokee-county-rusk-jacksonville-caddo-mounds-piney-woods-texas", "cherokee"],
   ["smith-county-tyler-roses-railroads-piney-woods-texas", "smith"],
   ["gregg-county-longview-kilgore-oil-railroads-piney-woods-texas", "gregg"],
+  ["upshur-county-gilmer-yamboree-piney-woods-texas", "upshur"],
 ]);
 
 const COUNTY_SLUGS = new Set(LEGACY_ARTICLE_TO_COUNTY.values());
