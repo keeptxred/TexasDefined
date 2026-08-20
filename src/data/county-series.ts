f58eba2e361c1e93f6d1a646add86d7dc32e6f23
keeptxred <freddy.coppola@gmail.com>
2026-08-19 23:05:48 -0500

    Map Fannin County legacy guide to canonical county page

--- src/data/county-series.ts
+++ src/data/county-series.ts
@@ -140,6 +140,7 @@ const LEGACY_ARTICLE_TO_COUNTY = new Map<string, string>([
   ["delta-county-cooper-sulphur-rivers-cooper-lake-blackland-prairie-texas", "delta"],
   ["hopkins-county-sulphur-springs-dairy-railroads-northeast-texas", "hopkins"],
   ["hunt-county-greenville-cotton-railroads-blackland-prairie-texas", "hunt"],
+  ["fannin-county-bonham-bois-darc-lake-rayburn-northeast-texas", "fannin"],
 ]);
 
 const COUNTY_SLUGS = new Set(LEGACY_ARTICLE_TO_COUNTY.values());
