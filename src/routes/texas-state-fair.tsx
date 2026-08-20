1aaf8a6443346b66afd4c67486c87c17baf52df2
keeptxred <freddy.coppola@gmail.com>
2026-08-20 17:02:24 -0500

    Add 2026 State Fair of Texas guide

---
+++ src/routes/texas-state-fair.tsx
@@ -0,0 +1,7 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { texasDefinedBrand } from "@/brand/texasdefined";
+import { PrioritySearchPage } from "@/components/editorial/PrioritySearchPage";
+import { PRIORITY_SEARCH_PAGES } from "@/data/priority-search-pages";
+import { buildMeta, canonicalLink } from "@/lib/seo";
+const path = "/texas-state-fair"; const data = PRIORITY_SEARCH_PAGES["texas-state-fair"];
+export const Route = createFileRoute(path)({ head: () => ({ meta: buildMeta(texasDefinedBrand,{canonicalPath:path,title:"State Fair of Texas 2026: Dates, Food, Rides & Planning",description:data.intro}), links:[canonicalLink(texasDefinedBrand,path)] }), component: () => <PrioritySearchPage data={data} /> });
