10ecaa5d537ac7ebd68e30dfae3814a1cbc3fe00
keeptxred <freddy.coppola@gmail.com>
2026-08-20 06:35:10 -0500

    Mark Foard County editorial guide complete

--- src/data/county-series.ts
+++ src/data/county-series.ts
@@ -154,6 +154,7 @@ const LEGACY_ARTICLE_TO_COUNTY = new Map<string, string>([
   ["clay-county-henrietta-red-river-lake-arrowhead-north-texas", "clay"],
   ["henderson-county-athens-lakes-fisheries-east-texas", "henderson"],
   ["wichita-county-wichita-falls-sheppard-oil-red-river-texas", "wichita"],
+  ["foard-county-crowell-pease-river-ranching-oil-rolling-plains-texas", "foard"],
 ]);
 
 const COUNTY_SLUGS = new Set(LEGACY_ARTICLE_TO_COUNTY.values());
