268a15313bbaaab8299bc83eca7a9e4f19fcca34
keeptxred <freddy.coppola@gmail.com>
2026-08-20 15:35:47 -0500

    Merge pre-index photography into canonical Painted Church gallery

--- src/data/painted-church-gallery-index.ts
+++ src/data/painted-church-gallery-index.ts
@@ -1,5 +1,6 @@
 import { extraPaintedChurchGalleryBySlug } from "./painted-church-gallery-extra";
 import { paintedChurchGalleryBySlug, type PaintedChurchGalleryImage } from "./painted-church-gallery";
+import { preindexPaintedChurchGalleryBySlug } from "./painted-church-gallery-preindex";
 import { supplementalPaintedChurchGalleryBySlug } from "./painted-church-gallery-supplemental";
 
 export function canonicalPaintedChurchGalleryBySlug(slug: string): PaintedChurchGalleryImage[] {
@@ -7,5 +8,6 @@ export function canonicalPaintedChurchGalleryBySlug(slug: string): PaintedChurch
     ...paintedChurchGalleryBySlug(slug),
     ...extraPaintedChurchGalleryBySlug(slug),
     ...supplementalPaintedChurchGalleryBySlug(slug),
+    ...preindexPaintedChurchGalleryBySlug(slug),
   ].map((image) => [image.sourceUrl, image])).values()];
 }
