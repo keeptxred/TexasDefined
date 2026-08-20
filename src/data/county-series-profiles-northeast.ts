37d975310912266802bc1217bef075602f992fbe
keeptxred <freddy.coppola@gmail.com>
2026-08-19 21:29:35 -0500

    Register Hopkins County editorial profile

---
+++ src/data/county-series-profiles-northeast.ts
@@ -0,0 +1,10 @@
+import type { CountySeriesProfile } from "@/data/county-series";
+
+export const COUNTY_SERIES_PROFILES_NORTHEAST: CountySeriesProfile[] = [
+  {
+    countySlug: "hopkins",
+    articleSlug: "hopkins-county-sulphur-springs-dairy-railroads-northeast-texas",
+    loadArticle: () => import("@/data/fixtures/hopkins-county-sulphur-springs-dairy-railroads-northeast-texas")
+      .then((module) => module.hopkinsCountySulphurSpringsDairyRailroadsNortheastTexasArticle),
+  },
+];
