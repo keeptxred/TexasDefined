b3ccfc75925399187bf5a37caebc650d58ac43af
keeptxred <freddy.coppola@gmail.com>
2026-08-21 11:12:07 -0500

    Redirect attraction mirrors to destination guides

---
+++ src/routes/attraction.$slug.tsx
@@ -0,0 +1,7 @@
+import { createFileRoute, redirect } from '@tanstack/react-router';
+
+export const Route = createFileRoute('/attraction/$slug')({
+  beforeLoad: ({ params }) => {
+    throw redirect({ href: `/destination/${params.slug}`, statusCode: 301 });
+  },
+});
