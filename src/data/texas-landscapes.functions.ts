ff53fc5b98c5393c2b34ffdb47613802bbdae471
keeptxred <freddy.coppola@gmail.com>
2026-08-21 12:15:34 -0500

    Load landscape catalogs through server functions

--- src/data/texas-landscapes.functions.ts
+++ src/data/texas-landscapes.functions.ts
@@ -1,5 +1,11 @@
 import { createServerFn } from '@tanstack/react-start';
 
+export const getTexasLandscapeHub = createServerFn({ method: 'GET' })
+  .handler(async () => {
+    const { loadTexasLandscapeHubServer } = await import('./texas-landscapes.server');
+    return loadTexasLandscapeHubServer();
+  });
+
 export const getTexasLandscapePage = createServerFn({ method: 'GET' })
   .inputValidator((data: { slug: string }) => data)
   .handler(async ({ data }) => {
