2f1ff612b30af9a3ba9176e6706b21a66ac8352a
keeptxred <freddy.coppola@gmail.com>
2026-08-21 03:02:52 -0500

    Register Potter County editorial profile

--- src/data/county-series-profiles-panhandle.ts
+++ src/data/county-series-profiles-panhandle.ts
@@ -7,4 +7,10 @@ export const COUNTY_SERIES_PROFILES_PANHANDLE: CountySeriesProfile[] = [
     loadArticle: () => import("@/data/fixtures/oldham-county-vega-tascosa-canadian-river-route-66-panhandle-texas")
       .then((module) => module.oldhamCountyVegaTascosaCanadianRiverRoute66PanhandleTexasArticle),
   },
+  {
+    countySlug: "potter",
+    articleSlug: "potter-county-amarillo-canadian-river-route-66-panhandle-texas",
+    loadArticle: () => import("@/data/fixtures/potter-county-amarillo-canadian-river-route-66-panhandle-texas")
+      .then((module) => module.potterCountyAmarilloCanadianRiverRoute66PanhandleTexasArticle),
+  },
 ];
