029e7893bebf64587d9c2f2672f87e73cf026f23
keeptxred <freddy.coppola@gmail.com>
2026-08-21 12:56:58 -0500

    Bundle showcase lake levels with page data

--- src/data/fishing/showcase-lakes-page-data.functions.ts
+++ src/data/fishing/showcase-lakes-page-data.functions.ts
@@ -1,7 +1,18 @@
 import { createServerFn } from "@tanstack/react-start";
 
+import { loadLiveLakeLevelResilient } from "./live-lake-level-fetch.server";
 import { loadShowcaseLakesPageDataServer } from "./showcase-lakes-page-data.server";
 
 export const getShowcaseLakesPageData = createServerFn({ method: "GET" }).handler(async () => {
-  return loadShowcaseLakesPageDataServer();
+  const pageData = loadShowcaseLakesPageDataServer();
+  const entries = await Promise.all(
+    Object.entries(pageData).map(async ([slug, lake]) => [
+      slug,
+      {
+        ...lake,
+        liveLakeLevel: await loadLiveLakeLevelResilient(lake.sources.liveLevel.url),
+      },
+    ] as const),
+  );
+  return Object.fromEntries(entries);
 });
