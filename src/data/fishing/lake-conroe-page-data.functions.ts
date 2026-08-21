a9bd8d6a414dfa789ec807d20d0dcd479fb24023
keeptxred <freddy.coppola@gmail.com>
2026-08-21 12:56:46 -0500

    Bundle Lake Conroe live level with page data

--- src/data/fishing/lake-conroe-page-data.functions.ts
+++ src/data/fishing/lake-conroe-page-data.functions.ts
@@ -1,7 +1,10 @@
 import { createServerFn } from "@tanstack/react-start";
 
 import { loadLakeConroePageDataServer } from "./lake-conroe-page-data.server";
+import { loadLiveLakeLevelResilient } from "./live-lake-level-fetch.server";
 
 export const getLakeConroePageData = createServerFn({ method: "GET" }).handler(async () => {
-  return loadLakeConroePageDataServer();
+  const pageData = loadLakeConroePageDataServer();
+  const liveLakeLevel = await loadLiveLakeLevelResilient(pageData.sources.liveLevel.url);
+  return { ...pageData, liveLakeLevel };
 });
