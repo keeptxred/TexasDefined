1f68232f5440c9109ed785d16ddb4b0497396733
keeptxred <freddy.coppola@gmail.com>
2026-08-22 09:04:24 -0500

    Register Coke County editorial profile

--- src/data/county-series-profiles-concho.ts
+++ src/data/county-series-profiles-concho.ts
@@ -13,4 +13,10 @@ export const COUNTY_SERIES_PROFILES_CONCHO: CountySeriesProfile[] = [
     loadArticle: () => import("@/data/fixtures/runnels-county-ballinger-winters-colorado-river-west-central-texas")
       .then((module) => module.runnelsCountyBallingerWintersColoradoRiverWestCentralTexasArticle),
   },
+  {
+    countySlug: "coke",
+    articleSlug: "coke-county-robert-lee-bronte-colorado-river-west-central-texas",
+    loadArticle: () => import("@/data/fixtures/coke-county-robert-lee-bronte-colorado-river-west-central-texas")
+      .then((module) => module.cokeCountyRobertLeeBronteColoradoRiverWestCentralTexasArticle),
+  },
 ];
