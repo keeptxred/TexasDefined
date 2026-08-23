090aafe61d7f75cf7665d8fb1647c598a2e5b822
keeptxred <freddy.coppola@gmail.com>
2026-08-22 20:02:42 -0500

    Defer landscape detail server function import

--- src/routes/explore.landscapes.$slug.tsx
+++ src/routes/explore.landscapes.$slug.tsx
@@ -1,9 +1,8 @@
 import { createFileRoute, notFound } from '@tanstack/react-router';
 
-import { getTexasLandscapePage } from '@/data/texas-landscapes.functions';
-
 export const Route = createFileRoute('/explore/landscapes/$slug')({
   loader: async ({ params }) => {
+    const { getTexasLandscapePage } = await import('@/data/texas-landscapes.functions');
     const result = await getTexasLandscapePage({ data: { slug: params.slug } });
     if (!result) throw notFound();
     return result;
