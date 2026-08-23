c353751cef1cefb350f9642282860a1369be9c3e
keeptxred <freddy.coppola@gmail.com>
2026-08-22 19:53:44 -0500

    Use route-level lazy split for landscape detail

--- src/routes/explore.landscapes.$slug.tsx
+++ src/routes/explore.landscapes.$slug.tsx
@@ -1,9 +1,6 @@
 import { createFileRoute, notFound } from '@tanstack/react-router';
-import { lazy, Suspense } from 'react';
 
 import { getTexasLandscapePage } from '@/data/texas-landscapes.functions';
-
-const TexasLandscapeDetailPage = lazy(() => import('@/components/editorial/TexasLandscapeDetailPage').then((module) => ({ default: module.TexasLandscapeDetailPage })));
 
 export const Route = createFileRoute('/explore/landscapes/$slug')({
   loader: async ({ params }) => {
@@ -12,14 +9,4 @@ export const Route = createFileRoute('/explore/landscapes/$slug')({
     return result;
   },
   head: ({ loaderData }) => loaderData?.head ?? {},
-  component: LandscapeDetailRoute,
 });
-
-function LandscapeDetailRoute() {
-  const { item, nearby } = Route.useLoaderData();
-  return (
-    <Suspense fallback={<div className="min-h-[36rem]" aria-hidden="true" />}>
-      <TexasLandscapeDetailPage item={item} nearby={nearby} />
-    </Suspense>
-  );
-}
