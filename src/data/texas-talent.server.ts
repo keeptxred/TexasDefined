4a44dad913b0da110cf124a6f8046cdf2a16b1af
keeptxred <freddy.coppola@gmail.com>
2026-08-25 17:50:21 -0500

    Add Texas Talent launch-readiness batch nine (#567)
    * Add ninth Texas Talent readiness batch
    
    * Attach ninth Texas Talent readiness batch

--- src/data/texas-talent.server.ts
+++ src/data/texas-talent.server.ts
@@ -9,6 +9,7 @@ import { TEXAS_TALENT_READINESS_BATCH5 } from "@/data/texas-talent-readiness-bat
 import { TEXAS_TALENT_READINESS_BATCH6 } from "@/data/texas-talent-readiness-batch6";
 import { TEXAS_TALENT_READINESS_BATCH7 } from "@/data/texas-talent-readiness-batch7";
 import { TEXAS_TALENT_READINESS_BATCH8 } from "@/data/texas-talent-readiness-batch8";
+import { TEXAS_TALENT_READINESS_BATCH9 } from "@/data/texas-talent-readiness-batch9";
 
 const TEXAS_TALENT_ALL_PROFILES = [
   ...TEXAS_TALENT_PROFILES,
@@ -25,6 +26,7 @@ const TEXAS_TALENT_ALL_READINESS = {
   ...TEXAS_TALENT_READINESS_BATCH6,
   ...TEXAS_TALENT_READINESS_BATCH7,
   ...TEXAS_TALENT_READINESS_BATCH8,
+  ...TEXAS_TALENT_READINESS_BATCH9,
 };
 
 function withReadiness<T extends (typeof TEXAS_TALENT_ALL_PROFILES)[number]>(profile: T) {
