fcd9064440235c3c6906d11c7c7272e617a4b4af
keeptxred <freddy.coppola@gmail.com>
2026-08-25 18:17:30 -0500

    Apply final readiness and profile corrections

--- src/data/texas-talent.server.ts
+++ src/data/texas-talent.server.ts
@@ -2,6 +2,7 @@ import { TEXAS_TALENT_PROFILES } from "@/data/texas-talent-profiles";
 import { TEXAS_TALENT_MUSIC_EXPANSION } from "@/data/texas-talent-profiles-wave2-music";
 import { TEXAS_TALENT_FILM_EXPANSION } from "@/data/texas-talent-profiles-wave2-film";
 import { TEXAS_TALENT_ARTS_EXPANSION } from "@/data/texas-talent-profiles-wave2-arts";
+import { TEXAS_TALENT_PROFILE_CORRECTIONS } from "@/data/texas-talent-profile-corrections";
 import { TEXAS_TALENT_READINESS } from "@/data/texas-talent-readiness";
 import { TEXAS_TALENT_READINESS_BATCH3 } from "@/data/texas-talent-readiness-batch3";
 import { TEXAS_TALENT_READINESS_BATCH4 } from "@/data/texas-talent-readiness-batch4";
@@ -10,6 +11,7 @@ import { TEXAS_TALENT_READINESS_BATCH6 } from "@/data/texas-talent-readiness-bat
 import { TEXAS_TALENT_READINESS_BATCH7 } from "@/data/texas-talent-readiness-batch7";
 import { TEXAS_TALENT_READINESS_BATCH8 } from "@/data/texas-talent-readiness-batch8";
 import { TEXAS_TALENT_READINESS_BATCH9 } from "@/data/texas-talent-readiness-batch9";
+import { TEXAS_TALENT_READINESS_BATCH10 } from "@/data/texas-talent-readiness-batch10";
 
 const TEXAS_TALENT_ALL_PROFILES = [
   ...TEXAS_TALENT_PROFILES,
@@ -27,11 +29,17 @@ const TEXAS_TALENT_ALL_READINESS = {
   ...TEXAS_TALENT_READINESS_BATCH7,
   ...TEXAS_TALENT_READINESS_BATCH8,
   ...TEXAS_TALENT_READINESS_BATCH9,
+  ...TEXAS_TALENT_READINESS_BATCH10,
 };
 
 function withReadiness<T extends (typeof TEXAS_TALENT_ALL_PROFILES)[number]>(profile: T) {
-  return {
+  const correctedProfile = {
     ...profile,
+    ...(TEXAS_TALENT_PROFILE_CORRECTIONS[profile.slug] ?? {}),
+  };
+
+  return {
+    ...correctedProfile,
     readiness: TEXAS_TALENT_ALL_READINESS[profile.slug] ?? null,
   };
 }
