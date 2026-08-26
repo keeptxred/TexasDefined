e1d720e8a1462ea464d0f422b172ce1491b2a47f
keeptxred <freddy.coppola@gmail.com>
2026-08-26 14:41:36 -0500

    Register verified Business ranks 181-190

--- src/data/texas-icons.server.ts
+++ src/data/texas-icons.server.ts
@@ -24,6 +24,7 @@ import { TEXAS_ICON_RESEARCH_SPORTS_BATCH_5 } from "@/data/texas-icons-research-
 import { TEXAS_ICON_RESEARCH_BUSINESS_BATCH_1 } from "@/data/texas-icons-research-business-1.server";
 import { TEXAS_ICON_RESEARCH_BUSINESS_BATCH_2 } from "@/data/texas-icons-research-business-2.server";
 import { TEXAS_ICON_RESEARCH_BUSINESS_BATCH_3 } from "@/data/texas-icons-research-business-3.server";
+import { TEXAS_ICON_RESEARCH_BUSINESS_BATCH_4 } from "@/data/texas-icons-research-business-4.server";
 import { isTexasTalentPublishable } from "@/data/texas-talent-launch";
 import { texasTalentFutureCanonicalPath } from "@/data/texas-talent-launch-metadata.server";
 import { loadTexasTalentProfilesServer } from "@/data/texas-talent.server";
@@ -63,6 +64,7 @@ const TEXAS_ICON_RESEARCH_PROFILES: readonly TexasIconResearchProfile[] = [
   ...TEXAS_ICON_RESEARCH_BUSINESS_BATCH_1,
   ...TEXAS_ICON_RESEARCH_BUSINESS_BATCH_2,
   ...TEXAS_ICON_RESEARCH_BUSINESS_BATCH_3,
+  ...TEXAS_ICON_RESEARCH_BUSINESS_BATCH_4,
 ];
 
 export type TexasIconReuseKind =
