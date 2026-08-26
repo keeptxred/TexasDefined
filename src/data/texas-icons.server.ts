93466cd477b0938c3d9dff9d908c7f97df66a834
keeptxred <freddy.coppola@gmail.com>
2026-08-25 23:15:45 -0500

    Wire validated Texas Icons batch 8 on current main

--- src/data/texas-icons.server.ts
+++ src/data/texas-icons.server.ts
@@ -8,6 +8,7 @@ import { TEXAS_ICON_RESEARCH_HISTORY_BATCH_4 } from "@/data/texas-icons-research
 import { TEXAS_ICON_RESEARCH_HISTORY_BATCH_5 } from "@/data/texas-icons-research-history-5.server";
 import { TEXAS_ICON_RESEARCH_HISTORY_BATCH_6 } from "@/data/texas-icons-research-history-6.server";
 import { TEXAS_ICON_RESEARCH_HISTORY_BATCH_7 } from "@/data/texas-icons-research-history-7.server";
+import { TEXAS_ICON_RESEARCH_HISTORY_BATCH_8 } from "@/data/texas-icons-research-history-8.server";
 import { isTexasTalentPublishable } from "@/data/texas-talent-launch";
 import { loadTexasTalentProfilesServer } from "@/data/texas-talent.server";
 import {
@@ -30,6 +31,7 @@ const TEXAS_ICON_RESEARCH_PROFILES: readonly TexasIconResearchProfile[] = [
   ...TEXAS_ICON_RESEARCH_HISTORY_BATCH_5,
   ...TEXAS_ICON_RESEARCH_HISTORY_BATCH_6,
   ...TEXAS_ICON_RESEARCH_HISTORY_BATCH_7,
+  ...TEXAS_ICON_RESEARCH_HISTORY_BATCH_8,
 ];
 
 export type TexasIconReuseKind =
