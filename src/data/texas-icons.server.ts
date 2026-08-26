c381cccf1ec314f43263e25c3e21310b8b15c8f6
keeptxred <freddy.coppola@gmail.com>
2026-08-26 13:12:02 -0500

    Register final Sports research batch in resolver

--- src/data/texas-icons.server.ts
+++ src/data/texas-icons.server.ts
@@ -20,6 +20,7 @@ import { TEXAS_ICON_RESEARCH_SPORTS_BATCH_1 } from "@/data/texas-icons-research-
 import { TEXAS_ICON_RESEARCH_SPORTS_BATCH_2 } from "@/data/texas-icons-research-sports-2.server";
 import { TEXAS_ICON_RESEARCH_SPORTS_BATCH_3 } from "@/data/texas-icons-research-sports-3.server";
 import { TEXAS_ICON_RESEARCH_SPORTS_BATCH_4 } from "@/data/texas-icons-research-sports-4.server";
+import { TEXAS_ICON_RESEARCH_SPORTS_BATCH_5 } from "@/data/texas-icons-research-sports-5.server";
 import { isTexasTalentPublishable } from "@/data/texas-talent-launch";
 import { texasTalentFutureCanonicalPath } from "@/data/texas-talent-launch-metadata.server";
 import { loadTexasTalentProfilesServer } from "@/data/texas-talent.server";
@@ -55,6 +56,7 @@ const TEXAS_ICON_RESEARCH_PROFILES: readonly TexasIconResearchProfile[] = [
   ...TEXAS_ICON_RESEARCH_SPORTS_BATCH_2,
   ...TEXAS_ICON_RESEARCH_SPORTS_BATCH_3,
   ...TEXAS_ICON_RESEARCH_SPORTS_BATCH_4,
+  ...TEXAS_ICON_RESEARCH_SPORTS_BATCH_5,
 ];
 
 export type TexasIconReuseKind =
