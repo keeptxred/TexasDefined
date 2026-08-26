ba7043ecbb8e85d362cea36827fe3927e97535bb
keeptxred <freddy.coppola@gmail.com>
2026-08-26 17:58:49 -0500

    Add Texas Music timeline route

---
+++ src/routes/texas-music-timeline.lazy.tsx
@@ -0,0 +1,4 @@
+import { createLazyFileRoute } from "@tanstack/react-router";
+import { TexasMusicTimeline } from "@/components/editorial/TexasMusicTimeline";
+
+export const Route = createLazyFileRoute("/texas-music-timeline")({ component: TexasMusicTimeline });
