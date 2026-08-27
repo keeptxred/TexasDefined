a84abf17639ca2d7d873c997c4c857e371220d75
keeptxred <freddy.coppola@gmail.com>
2026-08-26 22:29:27 -0500

    Publish fishing guide directory only with verified guides

--- src/data/fishing/guide-sitemap.server.ts
+++ src/data/fishing/guide-sitemap.server.ts
@@ -1,7 +1,10 @@
 import { fishingPlatform, fishingScope } from "./index";
-import { fishingGuideCanonicalPath } from "./guide-routing";
+import { FISHING_GUIDES_DIRECTORY_PATH, FISHING_GUIDES_VERIFIED_AT, fishingGuideCanonicalPath } from "./guide-routing";
 
 export async function loadFishingGuideSitemapEntriesServer() {
   const guides = await fishingPlatform.guides.list({ ...fishingScope, status: "published", verifiedListing: true, limit: 5000 });
-  return guides.map((guide) => ({ path: fishingGuideCanonicalPath(guide.slug), lastmod: guide.verifiedAt }));
+  return [
+    ...(guides.length ? [{ path: FISHING_GUIDES_DIRECTORY_PATH, lastmod: FISHING_GUIDES_VERIFIED_AT }] : []),
+    ...guides.map((guide) => ({ path: fishingGuideCanonicalPath(guide.slug), lastmod: guide.verifiedAt })),
+  ];
 }
