d6abfe19f4c1f159d546e9e5bf8639c3cadc3bf7
keeptxred <freddy.coppola@gmail.com>
2026-08-27 08:27:11 -0500

    Salvage server-backed major event authority guides

---
+++ src/routes/event.$slug.lazy.tsx
@@ -0,0 +1,17 @@
+import { createLazyFileRoute } from "@tanstack/react-router";
+
+export const Route = createLazyFileRoute("/event/$slug")({
+  component: MajorEventGuidePage,
+});
+
+function MajorEventGuidePage() {
+  const { page } = Route.useLoaderData();
+  return (
+    <>
+      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: page.jsonLd }} />
+      <main className="mx-auto max-w-4xl px-5 pb-20 pt-12 sm:px-8">
+        <article dangerouslySetInnerHTML={{ __html: page.html }} />
+      </main>
+    </>
+  );
+}
