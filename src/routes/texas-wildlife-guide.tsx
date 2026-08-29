c7ba3bf4ec1085bdafadfd6dfbf99a16cba598da
keeptxred <freddy.coppola@gmail.com>
2026-08-29 11:25:47 -0500

    Remove redundant wildlife alias route

--- src/routes/texas-wildlife-guide.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/texas-wildlife-guide")({
-  beforeLoad: () => {
-    throw redirect({ href: "/article/texas-wildlife-guide", statusCode: 301 });
-  },
-});
