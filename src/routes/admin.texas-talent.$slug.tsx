959d669a51764055e73ba2db860c4f19f2702cb4
keeptxred <freddy.coppola@gmail.com>
2026-08-29 10:55:02 -0500

    Reuse enriched Talent profile loader

--- src/routes/admin.texas-talent.$slug.tsx
+++ src/routes/admin.texas-talent.$slug.tsx
@@ -1,18 +1,12 @@
 import { createFileRoute, notFound } from "@tanstack/react-router";
 
-import {
-  getTexasTalentProfileWithResolvedLinks,
-  getTexasTalentRelatedProfiles,
-} from "@/data/texas-talent.functions";
+import { getTexasTalentProfileWithResolvedLinks } from "@/data/texas-talent.functions";
 
 export const Route = createFileRoute("/admin/texas-talent/$slug")({
   beforeLoad: async ({ params }) => {
-    const [profile, relatedProfiles] = await Promise.all([
-      getTexasTalentProfileWithResolvedLinks({ data: { slug: params.slug } }),
-      getTexasTalentRelatedProfiles({ data: { slug: params.slug, limit: 6 } }),
-    ]);
+    const profile = await getTexasTalentProfileWithResolvedLinks({ data: { slug: params.slug } });
     if (!profile) throw notFound();
-    return { profile, relatedProfiles };
+    return { profile, relatedProfiles: profile.relatedProfiles };
   },
   head: ({ match }) => ({
     meta: [
