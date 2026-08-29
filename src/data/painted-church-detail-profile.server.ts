ad6c08dcd16e875fbc086b9659c2e52af865e613
keeptxred <freddy.coppola@gmail.com>
2026-08-29 10:58:04 -0500

    Remove superseded Painted Church server resolver

--- src/data/painted-church-detail-profile.server.ts
+++
@@ -1,6 +0,0 @@
-import { paintedChurchExpansionProfileBySlug } from "./painted-church-profiles-expansion";
-import { latestPaintedChurchProfileBySlug } from "./painted-church-profiles-latest";
-
-export function loadPromotedPaintedChurchDetailProfileServer(slug: string) {
-  return paintedChurchExpansionProfileBySlug(slug) ?? latestPaintedChurchProfileBySlug(slug);
-}
