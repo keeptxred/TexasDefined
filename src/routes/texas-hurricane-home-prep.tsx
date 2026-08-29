1ae7ab9823d79022c445a8ff6ff801a6eb3ec794
keeptxred <freddy.coppola@gmail.com>
2026-08-29 11:25:37 -0500

    Remove redundant hurricane alias route

--- src/routes/texas-hurricane-home-prep.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/texas-hurricane-home-prep")({
-  beforeLoad: () => {
-    throw redirect({ href: "/article/texas-hurricane-preparation-homeowners-renters", statusCode: 301 });
-  },
-});
