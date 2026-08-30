451f0302008220bb6d6d6b12bec324db50b2f00f
keeptxred <freddy.coppola@gmail.com>
2026-08-30 14:39:29 -0500

    feat(property): render local tax calculator authority pages

---
+++ src/routes/property-tax-calculator.$location.lazy.tsx
@@ -0,0 +1,12 @@
+import { createLazyFileRoute } from '@tanstack/react-router';
+
+import { LocalPropertyTaxCalculatorPage } from '@/components/property/LocalPropertyTaxCalculatorPage';
+
+export const Route = createLazyFileRoute('/property-tax-calculator/$location')({
+  component: Page,
+});
+
+function Page() {
+  const { page } = Route.useLoaderData();
+  return <LocalPropertyTaxCalculatorPage profile={page.profile} />;
+}
