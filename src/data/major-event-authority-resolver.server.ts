bcb8327d114d03ed6ec004fe536cff404d9a1b3e
keeptxred <freddy.coppola@gmail.com>
2026-08-30 09:35:05 -0500

    Centralize server-only major-event resolvers

---
+++ src/data/major-event-authority-resolver.server.ts
@@ -0,0 +1,87 @@
+import { getMajorEventAuthorityServer } from "./major-event-authority.server";
+import { getExpandedMajorEventAuthorityServer } from "./major-event-expanded-authority.server";
+import { getExpandedMajorEventAuthorityTranche3Server } from "./major-event-expanded-authority-tranche3.server";
+import { getExpandedMajorEventAuthorityTranche4Server } from "./major-event-expanded-authority-tranche4.server";
+import { getExpandedMajorEventAuthorityTranche5Server } from "./major-event-expanded-authority-tranche5.server";
+import { getExpandedMajorEventAuthorityTranche6Server } from "./major-event-expanded-authority-tranche6.server";
+import { getExpandedMajorEventAuthorityTranche7Server } from "./major-event-expanded-authority-tranche7.server";
+import { getExpandedMajorEventAuthorityTranche8Server } from "./major-event-expanded-authority-tranche8.server";
+import { getExpandedMajorEventAuthorityTranche9Server } from "./major-event-expanded-authority-tranche9.server";
+import { getExpandedMajorEventAuthorityTranche10Server } from "./major-event-expanded-authority-tranche10.server";
+import { getExpandedMajorEventAuthorityTranche11Server } from "./major-event-expanded-authority-tranche11.server";
+import { getExpandedMajorEventAuthorityTranche12Server } from "./major-event-expanded-authority-tranche12.server";
+import { getExpandedMajorEventAuthorityTranche13Server } from "./major-event-expanded-authority-tranche13.server";
+import { getExpandedMajorEventAuthorityTranche14Server } from "./major-event-expanded-authority-tranche14.server";
+import { getExpandedMajorEventAuthorityTranche15Server } from "./major-event-expanded-authority-tranche15.server";
+import { getExpandedMajorEventAuthorityTranche16Server } from "./major-event-expanded-authority-tranche16.server";
+import { getExpandedMajorEventAuthorityTranche17Server } from "./major-event-expanded-authority-tranche17.server";
+import { getExpandedMajorEventAuthorityTranche18Server } from "./major-event-expanded-authority-tranche18.server";
+import { getExpandedMajorEventAuthorityTranche19Server } from "./major-event-expanded-authority-tranche19.server";
+import { getExpandedMajorEventAuthorityTranche20Server } from "./major-event-expanded-authority-tranche20.server";
+import { getExpandedMajorEventAuthorityTranche21Server } from "./major-event-expanded-authority-tranche21.server";
+import { getExpandedMajorEventAuthorityTranche22Server } from "./major-event-expanded-authority-tranche22.server";
+import { getExpandedMajorEventAuthorityTranche23Server } from "./major-event-expanded-authority-tranche23.server";
+import { getExpandedMajorEventAuthorityTranche24Server } from "./major-event-expanded-authority-tranche24.server";
+import { getExpandedMajorEventAuthorityTranche25Server } from "./major-event-expanded-authority-tranche25.server";
+import { getExpandedMajorEventAuthorityTranche26Server } from "./major-event-expanded-authority-tranche26.server";
+import { getExpandedMajorEventAuthorityTranche27Server } from "./major-event-expanded-authority-tranche27.server";
+import { getExpandedMajorEventAuthorityTranche28Server } from "./major-event-expanded-authority-tranche28.server";
+import { getExpandedMajorEventAuthorityTranche29Server } from "./major-event-expanded-authority-tranche29.server";
+import { getExpandedMajorEventAuthorityTranche30Server } from "./major-event-expanded-authority-tranche30.server";
+import { getExpandedMajorEventAuthorityTranche31Server } from "./major-event-expanded-authority-tranche31.server";
+import { getExpandedMajorEventAuthorityTranche32Server } from "./major-event-expanded-authority-tranche32.server";
+import { getExpandedMajorEventAuthorityTranche33Server } from "./major-event-expanded-authority-tranche33.server";
+import { getExpandedMajorEventAuthorityTranche34Server } from "./major-event-expanded-authority-tranche34.server";
+import { getExpandedMajorEventAuthorityTranche35Server } from "./major-event-expanded-authority-tranche35.server";
+import { getExpandedMajorEventAuthorityTranche36Server } from "./major-event-expanded-authority-tranche36.server";
+import { getExpandedMajorEventAuthorityTranche37Server } from "./major-event-expanded-authority-tranche37.server";
+import { getExpandedMajorEventAuthorityTranche38Server } from "./major-event-expanded-authority-tranche38.server";
+import { getExpandedMajorEventAuthorityTranche39Server } from "./major-event-expanded-authority-tranche39.server";
+import { getExpandedMajorEventAuthorityTranche40Server } from "./major-event-expanded-authority-tranche40.server";
+import { getExpandedMajorEventAuthorityTranche41Server } from "./major-event-expanded-authority-tranche41.server";
+import { getExpandedMajorEventAuthorityTranche42Server } from "./major-event-expanded-authority-tranche42.server";
+
+export function resolveMajorEventAuthorityServer(slug: string) {
+  return getMajorEventAuthorityServer(slug)
+    ?? getExpandedMajorEventAuthorityServer(slug)
+    ?? getExpandedMajorEventAuthorityTranche3Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche4Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche5Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche6Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche7Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche8Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche9Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche10Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche11Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche12Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche13Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche14Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche15Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche16Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche17Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche18Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche19Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche20Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche21Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche22Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche23Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche24Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche25Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche26Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche27Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche28Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche29Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche30Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche31Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche32Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche33Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche34Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche35Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche36Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche37Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche38Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche39Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche40Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche41Server(slug)
+    ?? getExpandedMajorEventAuthorityTranche42Server(slug);
+}
