4c81bbc2747947463d506d05c39aaa61e05a0836
keeptxred <freddy.coppola@gmail.com>
2026-09-02 07:53:15 -0500

    Route event county links to canonical guides

--- src/routes/event.$slug.tsx
+++ src/routes/event.$slug.tsx
@@ -3,11 +3,23 @@ import { texasDefinedBrand } from "@/brand/texasdefined";
 import { getMajorEventAuthority } from "@/data/major-event-authority";
 import { buildMeta, canonicalLink } from "@/lib/seo";
 
+function canonicalizeMajorEventCountyLinks(html: string) {
+  return html.replace(
+    /href="\/browse\/counties#county-([a-z0-9-]+)"/g,
+    'href="/county/$1"',
+  );
+}
+
 export const Route = createFileRoute("/event/$slug")({
   loader: async ({ params }) => {
     const page = await getMajorEventAuthority(params.slug);
     if (!page) throw notFound();
-    return { page };
+    return {
+      page: {
+        ...page,
+        html: canonicalizeMajorEventCountyLinks(page.html),
+      },
+    };
   },
   head: ({ loaderData }) => {
     if (!loaderData) return {};
