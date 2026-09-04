dc3373aa4539e303f7dd3c06f9d5a512faebc738
keeptxred <freddy.coppola@gmail.com>
2026-09-04 11:09:45 -0500

    Trim Texas Dogs breed route overhead

--- src/routes/dogs.$breed.tsx
+++ src/routes/dogs.$breed.tsx
@@ -1,9 +1,7 @@
-import { lazy, Suspense } from 'react';
 import { createFileRoute, notFound } from '@tanstack/react-router';
 
+import DogBreedPage from '@/components/dogs/DogBreedPage';
 import { loadDogBreedPage } from '@/data/texas-dogs';
-
-const DogBreedPage = lazy(() => import('@/components/dogs/DogBreedPage'));
 
 export const Route = createFileRoute('/dogs/$breed')({
   loader: async ({ params }) => {
@@ -12,9 +10,5 @@ export const Route = createFileRoute('/dogs/$breed')({
     return data;
   },
   head: ({ loaderData }) => loaderData?.head ?? {},
-  component: DogBreedRouteComponent,
+  component: DogBreedPage,
 });
-
-function DogBreedRouteComponent() {
-  return <Suspense fallback={<div className="min-h-[40vh]" aria-busy="true" />}><DogBreedPage /></Suspense>;
-}
