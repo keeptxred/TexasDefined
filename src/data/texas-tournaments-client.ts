21f9c251a668b7555b85750e006e3124a3e9264f
keeptxred <freddy.coppola@gmail.com>
2026-09-04 19:44:18 -0500

    Remove county tournament client server function

--- src/data/texas-tournaments-client.ts
+++ src/data/texas-tournaments-client.ts
@@ -1,22 +1,11 @@
 import { createServerFn } from '@tanstack/react-start';
 
-const loadCountyTournamentSeeds = createServerFn({ method: 'GET' })
-  .inputValidator((data: { countySlug: string }) => data)
-  .handler(async ({ data }) => {
-    const { loadCountyTournamentSeedsServer } = await import('./texas-tournaments.server');
-    return loadCountyTournamentSeedsServer(data.countySlug);
-  });
-
 const loadTournamentEntity = createServerFn({ method: 'GET' })
   .inputValidator((data: { slug: string }) => data)
   .handler(async ({ data }) => {
     const { loadTournamentEntityServer } = await import('./texas-tournaments.server');
     return loadTournamentEntityServer(data.slug);
   });
-
-export function getCountyTournamentSeeds(countySlug: string) {
-  return loadCountyTournamentSeeds({ data: { countySlug } });
-}
 
 export function getTournamentEntity(slug: string) {
   return loadTournamentEntity({ data: { slug } });
