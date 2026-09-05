a9c3be05401c6059e82b62e4c4a88cdae2075b8a
keeptxred <freddy.coppola@gmail.com>
2026-09-05 13:02:30 -0500

    Add Expedia stays to major event guides

--- src/routes/event.$slug.lazy.tsx
+++ src/routes/event.$slug.lazy.tsx
@@ -1,5 +1,7 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
 
+import { ExpediaStaySearch } from "@/components/affiliate/ExpediaStaySearch";
+
 export const Route = createLazyFileRoute("/event/$slug")({
   component: MajorEventGuidePage,
 });
@@ -11,6 +13,13 @@ function MajorEventGuidePage() {
       <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: page.jsonLd }} />
       <main className="mx-auto max-w-4xl px-5 pb-20 pt-12 sm:px-8">
         <article dangerouslySetInnerHTML={{ __html: page.html }} />
+        <ExpediaStaySearch
+          id={`expedia-event-${page.slug}-stays`}
+          compact
+          locationLabel="Texas"
+          title="Find a place to stay for the event"
+          description={`Compare current hotel and lodging options while planning an overnight trip around ${page.title}.`}
+        />
       </main>
     </>
   );
