7292fdb2044b6f1e356c245adcc422a72de2003e
keeptxred <freddy.coppola@gmail.com>
2026-09-05 13:35:58 -0500

    Trim Expedia event placement

--- src/routes/event.$slug.lazy.tsx
+++ src/routes/event.$slug.lazy.tsx
@@ -7,20 +7,13 @@ export const Route = createLazyFileRoute("/event/$slug")({
 });
 
 function MajorEventGuidePage() {
-  const { slug } = Route.useParams();
   const { page } = Route.useLoaderData();
   return (
     <>
       <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: page.jsonLd }} />
       <main className="mx-auto max-w-4xl px-5 pb-20 pt-12 sm:px-8">
         <article dangerouslySetInnerHTML={{ __html: page.html }} />
-        <ExpediaStaySearch
-          id={`expedia-event-${slug}-stays`}
-          compact
-          locationLabel="Texas"
-          title="Find a place to stay for the event"
-          description={`Compare current hotel and lodging options while planning an overnight trip around ${page.title}.`}
-        />
+        <ExpediaStaySearch />
       </main>
     </>
   );
