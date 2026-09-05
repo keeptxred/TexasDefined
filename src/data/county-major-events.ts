11a6d2feb74c26fa99ad82c906a4c73eba98832a
keeptxred <freddy.coppola@gmail.com>
2026-09-05 18:08:03 -0500

    Consolidate county planning server boundary

--- src/data/county-major-events.ts
+++ src/data/county-major-events.ts
@@ -3,8 +3,14 @@ import { createServerFn } from "@tanstack/react-start";
 const loadCountyMajorEvents = createServerFn({ method: "GET" })
   .inputValidator((data: { countySlug: string }) => data)
   .handler(async ({ data }) => {
-    const { loadCountyMajorEventsServer } = await import("./county-major-events.server");
-    return loadCountyMajorEventsServer(data.countySlug);
+    const [{ loadCountyMajorEventsServer }, { loadCountyRvCampingServer }] = await Promise.all([
+      import("./county-major-events.server"),
+      import("./county-rv-camping.server"),
+    ]);
+    return {
+      majorEvents: loadCountyMajorEventsServer(data.countySlug),
+      rvCamping: loadCountyRvCampingServer(data.countySlug),
+    };
   });
 
 export function getCountyMajorEvents(countySlug: string) {
