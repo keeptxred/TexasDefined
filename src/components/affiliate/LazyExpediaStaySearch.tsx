a13b6bd2376f998972736072929d241caba02041
keeptxred <freddy.coppola@gmail.com>
2026-09-05 13:31:52 -0500

    Remove redundant Expedia lazy wrapper

--- src/components/affiliate/LazyExpediaStaySearch.tsx
+++
@@ -1,8 +0,0 @@
-import { lazy, Suspense } from 'react';
-import type { ExpediaStaySearchProps } from './ExpediaStaySearch';
-
-const Search = lazy(() => import('./ExpediaStaySearch').then(({ ExpediaStaySearch }) => ({ default: ExpediaStaySearch })));
-
-export function LazyExpediaStaySearch(props: ExpediaStaySearchProps) {
-  return <Suspense fallback={null}><Search {...props} /></Suspense>;
-}
