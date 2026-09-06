c44930b180c0213145174c7247d53576f8bae0d2
keeptxred <freddy.coppola@gmail.com>
2026-09-06 09:00:18 -0500

    Install Google Tag Manager sitewide

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -23,6 +23,31 @@ const siteUrl = `https://${texasDefinedBrand.identity.domain}`;
 const defaultSocialImage = absoluteUrl(texasDefinedBrand, heroHillCountry);
 const defaultSocialImageAlt = "Texas Hill Country landscape at golden hour";
 const iconVersion = "20260822";
+const googleTagManagerId = "GTM-5DK7GCGV";
+
+function GoogleTagManagerHead() {
+  return (
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${googleTagManagerId}');`,
+      }}
+    />
+  );
+}
+
+function GoogleTagManagerNoScript() {
+  return (
+    <noscript>
+      <iframe
+        src={`https://www.googletagmanager.com/ns.html?id=${googleTagManagerId}`}
+        height="0"
+        width="0"
+        style={{ display: "none", visibility: "hidden" }}
+        title="Google Tag Manager"
+      />
+    </noscript>
+  );
+}
 
 function NotFoundComponent() {
   return <Suspense fallback={null}><NotFoundScreen /></Suspense>;
@@ -131,9 +156,9 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
 
 function RootShell({ children }: { children: ReactNode }) {
   if (import.meta.env.SSR) {
-    return <html lang={texasDefinedBrand.identity.locale}><head><HeadContent /></head><body>{children}<Scripts /><script src="/expedia-travel.js" defer /></body></html>;
+    return <html lang={texasDefinedBrand.identity.locale}><head><GoogleTagManagerHead /><HeadContent /></head><body><GoogleTagManagerNoScript />{children}<Scripts /><script src="/expedia-travel.js" defer /></body></html>;
   }
-  return <html lang={texasDefinedBrand.identity.locale}><head><HeadContent /></head><body>{children}<Scripts /></body></html>;
+  return <html lang={texasDefinedBrand.identity.locale}><head><GoogleTagManagerHead /><HeadContent /></head><body><GoogleTagManagerNoScript />{children}<Scripts /></body></html>;
 }
 
 function RootComponent() {
