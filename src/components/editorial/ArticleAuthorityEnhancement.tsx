7694160e084b9b5c4daae16f514baa5a09b40639
keeptxred <freddy.coppola@gmail.com>
2026-09-05 21:22:08 -0500

    Remove superseded article authority wrapper

--- src/components/editorial/ArticleAuthorityEnhancement.tsx
+++
@@ -1,9 +0,0 @@
-import { MetroRelocationAuthority } from "@/components/relocation/MetroRelocationAuthority";
-import { WildflowerSpeciesGrid } from "@/components/editorial/WildflowerSpeciesGrid";
-
-const WILDFLOWER_GUIDE_PATH = "/article/texas-wildflowers-guide";
-
-export function ArticleAuthorityEnhancement({ articlePath }: { articlePath: string }) {
-  if (articlePath === WILDFLOWER_GUIDE_PATH) return <WildflowerSpeciesGrid />;
-  return <MetroRelocationAuthority articlePath={articlePath} />;
-}
