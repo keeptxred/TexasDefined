7a7592d648f3f86c869e7fb88e1c3f5bf855db62
keeptxred <freddy.coppola@gmail.com>
2026-09-07 00:10:33 -0500

    Remove failed standalone county RV server function

--- src/data/rv-parks/county.functions.ts
+++
@@ -1,8 +0,0 @@
-import { createServerFn } from "@tanstack/react-start";
-
-export const loadCountyRvParks = createServerFn({ method: "GET" })
-  .inputValidator((data: { countySlug: string }) => data)
-  .handler(async ({ data }) => {
-    const registry = await import("./registry.server");
-    return registry.loadRvParksForCountyServer(data.countySlug).slice(0, 12);
-  });
