8e43c4d5386cf7442aadc749fa0b939294e26276
keeptxred <freddy.coppola@gmail.com>
2026-09-08 17:25:48 -0500

    Curate Viator Texas inventory pages 21-23

--- src/data/viator-curated-product-seeds.ts
+++ src/data/viator-curated-product-seeds.ts
@@ -4,6 +4,7 @@ import {
   VIATOR_PRODUCT_EXCLUSION_RULES,
 } from "./viator-curated-product-seeds-base";
 import { VIATOR_CURATED_PRODUCT_SEEDS_PAGES_18_20 } from "./viator-curated-product-seeds-pages18-20";
+import { VIATOR_CURATED_PRODUCT_SEEDS_PAGES_21_23 } from "./viator-curated-product-seeds-pages21-23";
 
 export type { ViatorProductSeed, ViatorProductSeedFit } from "./viator-curated-product-seeds-base";
 export { VIATOR_PRODUCT_EXCLUSION_RULES };
@@ -15,6 +16,7 @@ export { VIATOR_PRODUCT_EXCLUSION_RULES };
 export const VIATOR_CURATED_PRODUCT_SEEDS: readonly ViatorProductSeed[] = [
   ...VIATOR_CURATED_PRODUCT_SEEDS_BASE,
   ...VIATOR_CURATED_PRODUCT_SEEDS_PAGES_18_20,
+  ...VIATOR_CURATED_PRODUCT_SEEDS_PAGES_21_23,
 ];
 
 export function viatorSeedsForMarket(marketSlug: string) {
