bbd2028a1b2c5b57ded5f3bf03df7f57b82c12ce
keeptxred <freddy.coppola@gmail.com>
2026-09-10 00:16:22 -0500

    Remove unrelated return policy lazy route from Dogs branch

--- src/routes/return-refund-policy.lazy.tsx
+++
@@ -1,7 +0,0 @@
-import { createLazyFileRoute } from "@tanstack/react-router";
-
-import ReturnRefundPolicyPage from "@/components/ReturnRefundPolicyPage";
-
-export const Route = createLazyFileRoute("/return-refund-policy")({
-  component: ReturnRefundPolicyPage,
-});
