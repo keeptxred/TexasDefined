1109e3335ae58b7a94371f488b623121ee0df72e
keeptxred <freddy.coppola@gmail.com>
2026-09-09 20:33:29 -0500

    Use static Dogs server-function bridge in route shell

--- src/routes/dogs.{-$breed}.tsx
+++ src/routes/dogs.{-$breed}.tsx
@@ -1,7 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import { loadDogsPage } from "@/data/texas-dogs.functions";
+
 // Static governance marker for the server-built head: canonicalPath, title: and description are returned in loaderData.head for both the hub and breed pages.
 export const Route = createFileRoute("/dogs/{-$breed}")({
-  loader: ({ params }) => import("@/data/texas-dogs.functions").then((m) => m.loadDogsPage(params.breed)),
+  loader: ({ params }) => loadDogsPage(params.breed),
   head: ({ loaderData }) => loaderData?.head ?? {},
 });
