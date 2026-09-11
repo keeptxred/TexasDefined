93cefc632ed6b62e359555b77240f7ce07f0d949
keeptxred <freddy.coppola@gmail.com>
2026-09-11 08:36:14 -0500

    Load verified parking maps for event pages

--- src/routes/event.$slug.tsx
+++ src/routes/event.$slug.tsx
@@ -1,13 +1,17 @@
 import { createFileRoute, notFound } from "@tanstack/react-router";
 import { texasDefinedBrand } from "@/brand/texasdefined";
 import { getMajorEventAuthority } from "@/data/major-event-authority";
+import { getMajorEventParkingMap } from "@/data/parking-maps.server";
 import { buildMeta, canonicalLink } from "@/lib/seo";
 
 export const Route = createFileRoute("/event/$slug")({
   loader: async ({ params }) => {
-    const page = await getMajorEventAuthority(params.slug);
+    const [page, parkingMap] = await Promise.all([
+      getMajorEventAuthority(params.slug),
+      getMajorEventParkingMap(params.slug),
+    ]);
     if (!page) throw notFound();
-    return { page };
+    return { page, parkingMap };
   },
   head: ({ loaderData }) => {
     if (!loaderData) return {};
