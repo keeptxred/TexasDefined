3989478ac1e57aae941279e93c72e0190ba7dcdc
keeptxred <freddy.coppola@gmail.com>
2026-09-11 20:25:07 -0500

    Compose final licensed venue photo wave

--- src/data/sports-venue-images-all.ts
+++ src/data/sports-venue-images-all.ts
@@ -1,10 +1,11 @@
 import { getSportsVenuePhotoAddition } from './sports-venue-images-additions';
 import { getSportsVenuePhotoAdditionWave2 } from './sports-venue-images-additions-wave2';
 import { getSportsVenuePhotoAdditionWave3 } from './sports-venue-images-additions-wave3';
+import { getSportsVenuePhotoAdditionWave4 } from './sports-venue-images-additions-wave4';
 import { getSportsVenuePhoto as getSportsVenuePhotoBase } from './sports-venue-images';
 
 export type { SportsVenuePhoto } from './sports-venue-images';
 
 export function getSportsVenuePhoto(slug: string) {
-  return getSportsVenuePhotoBase(slug) ?? getSportsVenuePhotoAddition(slug) ?? getSportsVenuePhotoAdditionWave2(slug) ?? getSportsVenuePhotoAdditionWave3(slug);
+  return getSportsVenuePhotoBase(slug) ?? getSportsVenuePhotoAddition(slug) ?? getSportsVenuePhotoAdditionWave2(slug) ?? getSportsVenuePhotoAdditionWave3(slug) ?? getSportsVenuePhotoAdditionWave4(slug);
 }
