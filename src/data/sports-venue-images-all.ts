41a3af0f9941b6f55c08e1acb8a78be274dfbfef
keeptxred <freddy.coppola@gmail.com>
2026-09-11 20:23:34 -0500

    Compose UFCU Stadium photo wave

--- src/data/sports-venue-images-all.ts
+++ src/data/sports-venue-images-all.ts
@@ -1,9 +1,10 @@
 import { getSportsVenuePhotoAddition } from './sports-venue-images-additions';
 import { getSportsVenuePhotoAdditionWave2 } from './sports-venue-images-additions-wave2';
+import { getSportsVenuePhotoAdditionWave3 } from './sports-venue-images-additions-wave3';
 import { getSportsVenuePhoto as getSportsVenuePhotoBase } from './sports-venue-images';
 
 export type { SportsVenuePhoto } from './sports-venue-images';
 
 export function getSportsVenuePhoto(slug: string) {
-  return getSportsVenuePhotoBase(slug) ?? getSportsVenuePhotoAddition(slug) ?? getSportsVenuePhotoAdditionWave2(slug);
+  return getSportsVenuePhotoBase(slug) ?? getSportsVenuePhotoAddition(slug) ?? getSportsVenuePhotoAdditionWave2(slug) ?? getSportsVenuePhotoAdditionWave3(slug);
 }
