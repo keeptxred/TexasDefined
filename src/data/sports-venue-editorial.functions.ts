0a84a7db7676b9171bd54526ad681e4a39add8a5
keeptxred <freddy.coppola@gmail.com>
2026-09-12 16:14:50 -0500

    Add bulk sports venue editorial lookup

--- src/data/sports-venue-editorial.functions.ts
+++ src/data/sports-venue-editorial.functions.ts
@@ -6,3 +6,15 @@ export const getSportsVenueEditorialDescription = createServerFn({ method: 'GET'
     const { getSportsVenueEditorialDescriptionServer } = await import('./sports-venue-editorial.server');
     return getSportsVenueEditorialDescriptionServer(data.id);
   });
+
+export const getSportsVenueEditorialDescriptions = createServerFn({ method: 'GET' })
+  .inputValidator((data: { ids: string[] }) => data)
+  .handler(async ({ data }) => {
+    const { getSportsVenueEditorialDescriptionServer } = await import('./sports-venue-editorial.server');
+    const descriptions: Record<string, string> = {};
+    for (const id of new Set(data.ids)) {
+      const description = getSportsVenueEditorialDescriptionServer(id);
+      if (description) descriptions[id] = description;
+    }
+    return descriptions;
+  });
