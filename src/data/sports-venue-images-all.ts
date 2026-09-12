9147bd5d2734e9367570bbf50ddf06accdc2f967
keeptxred <freddy.coppola@gmail.com>
2026-09-11 20:20:05 -0500

    Compose second venue photo addition wave

--- src/data/sports-venue-images-all.ts
+++ src/data/sports-venue-images-all.ts
@@ -1,8 +1,9 @@
 import { getSportsVenuePhotoAddition } from './sports-venue-images-additions';
+import { getSportsVenuePhotoAdditionWave2 } from './sports-venue-images-additions-wave2';
 import { getSportsVenuePhoto as getSportsVenuePhotoBase } from './sports-venue-images';
 
 export type { SportsVenuePhoto } from './sports-venue-images';
 
 export function getSportsVenuePhoto(slug: string) {
-  return getSportsVenuePhotoBase(slug) ?? getSportsVenuePhotoAddition(slug);
+  return getSportsVenuePhotoBase(slug) ?? getSportsVenuePhotoAddition(slug) ?? getSportsVenuePhotoAdditionWave2(slug);
 }
