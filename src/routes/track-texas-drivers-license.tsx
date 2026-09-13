09f6089efbe29158cee6b519db56813e6ee36041
keeptxred <freddy.coppola@gmail.com>
2026-09-13 10:09:37 -0500

    Move driver license tracking rendering to lazy route

--- src/routes/track-texas-drivers-license.tsx
+++ src/routes/track-texas-drivers-license.tsx
@@ -1,5 +1,4 @@
 import { createFileRoute, notFound } from "@tanstack/react-router";
-import { PrioritySearchPage } from "@/components/editorial/PrioritySearchPage";
 import { loadPrioritySearchPage } from "@/data/priority-search-page";
 import { buildPrioritySearchHead } from "@/lib/priority-search-seo";
 
@@ -18,9 +17,4 @@ export const Route = createFileRoute("/track-texas-drivers-license")({
     data: loaderData,
     about: ["Texas driver license status", "Texas DPS", "driver license mailing status", "Texas identification card"],
   }) : {},
-  component: Page,
 });
-
-function Page() {
-  return <PrioritySearchPage data={Route.useLoaderData()} />;
-}
