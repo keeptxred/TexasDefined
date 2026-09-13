cc4e70730f0cd95d8b5657dee7ebd4bae2491e81
keeptxred <freddy.coppola@gmail.com>
2026-09-12 21:29:10 -0500

    Expose utilities finder search title

--- src/routes/find-my-utilities.tsx
+++ src/routes/find-my-utilities.tsx
@@ -10,7 +10,7 @@ const siteUrl = `https://${texasDefinedBrand.identity.domain}`;
 
 export const Route = createFileRoute(canonicalPath)({
   head: () => ({
-    meta: buildMeta(texasDefinedBrand, { canonicalPath, title, description }),
+    meta: buildMeta(texasDefinedBrand, { canonicalPath, title: 'Find Utilities for a Texas Address', description }),
     links: [canonicalLink(texasDefinedBrand, canonicalPath)],
     scripts: [jsonLd({ '@context': 'https://schema.org', '@type': 'WebApplication', name: title, description, url: `${siteUrl}${canonicalPath}`, applicationCategory: 'ReferenceApplication', operatingSystem: 'Web' })],
   }),
