402df9c1fef87817e67e4d4d0170c88d28ebd865
keeptxred <freddy.coppola@gmail.com>
2026-09-12 21:29:44 -0500

    Expose property tax finder search title

--- src/routes/find-my-property-tax.tsx
+++ src/routes/find-my-property-tax.tsx
@@ -10,7 +10,7 @@ const siteUrl = `https://${texasDefinedBrand.identity.domain}`;
 
 export const Route = createFileRoute(canonicalPath)({
   head: () => ({
-    meta: buildMeta(texasDefinedBrand, { canonicalPath, title, description }),
+    meta: buildMeta(texasDefinedBrand, { canonicalPath, title: 'Find Your Texas Property Tax and Appraisal Offices', description }),
     links: [canonicalLink(texasDefinedBrand, canonicalPath)],
     scripts: [jsonLd({ '@context': 'https://schema.org', '@type': 'WebApplication', name: title, description, url: `${siteUrl}${canonicalPath}`, applicationCategory: 'FinanceApplication', operatingSystem: 'Web' })],
   }),
