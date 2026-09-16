f1368b7e86693ea406f39f5d4767b2afb36c5b4d
keeptxred <freddy.coppola@gmail.com>
2026-09-15 23:37:27 -0500

    Reconcile partner referral reporting with latest main

---
+++ src/data/partner-referral-analytics.types.ts
@@ -0,0 +1,39 @@
+export type PartnerReferralBreakdown = {
+  key: string;
+  label: string;
+  clicks30d: number;
+  clicks7d: number;
+};
+
+export type PartnerReferralPageBreakdown = {
+  pagePath: string;
+  clicks30d: number;
+  clicks7d: number;
+};
+
+export type PartnerReferralDestinationBreakdown = {
+  partner: string;
+  destinationUrl: string;
+  clicks30d: number;
+  clicks7d: number;
+};
+
+export type PartnerReferralDailyPoint = {
+  date: string;
+  clicks: number;
+};
+
+export type PartnerReferralAnalyticsDashboard = {
+  generatedAt: string;
+  lastSyncedAt: string | null;
+  windowDays: number;
+  totalClicks30d: number;
+  totalClicks7d: number;
+  prior7dClicks: number;
+  weekOverWeekPercent: number | null;
+  partners: PartnerReferralBreakdown[];
+  placements: PartnerReferralBreakdown[];
+  pages: PartnerReferralPageBreakdown[];
+  destinations: PartnerReferralDestinationBreakdown[];
+  daily: PartnerReferralDailyPoint[];
+};
