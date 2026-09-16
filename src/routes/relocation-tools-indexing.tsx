379d4ac4fcfb50b4b32bb691ad7e297cc9389f54
keeptxred <freddy.coppola@gmail.com>
2026-09-16 07:12:40 -0500

    Remove temporary indexing route

--- src/routes/relocation-tools-indexing.tsx
+++
@@ -1,2 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-export const Route=createFileRoute('/relocation-tools-indexing')({beforeLoad:()=>{throw new Response(null,{status:404})}});
