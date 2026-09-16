0a2e50c3347870eb556f92135a254ca28e794db3
keeptxred <freddy.coppola@gmail.com>
2026-09-16 11:43:11 -0500

    Make advertising aliases permanent redirects

--- src/routes/advertising-terms.tsx
+++ src/routes/advertising-terms.tsx
@@ -2,6 +2,6 @@ import { createFileRoute, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/advertising-terms')({
   beforeLoad: () => {
-    throw redirect({ to: '/partner-with-us/terms', replace: true });
+    throw redirect({ to: '/partner-with-us/terms', replace: true, statusCode: 301 });
   },
 });
