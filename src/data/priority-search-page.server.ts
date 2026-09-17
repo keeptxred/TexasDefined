1ad3b460e9944d64811e4300418bc71cc105e324
keeptxred <freddy.coppola@gmail.com>
2026-09-17 17:03:33 -0500

    Load Education authority pages server-side

--- src/data/priority-search-page.server.ts
+++ src/data/priority-search-page.server.ts
@@ -1,9 +1,11 @@
+import { EDUCATION_PRIORITY_SEARCH_PAGES } from "./priority-search-pages-education";
 import { PRIORITY_SEARCH_PAGES } from "./priority-search-pages";
 import { CURRENT_PUBLIC_SERVICE_PRIORITY_SEARCH_PAGES } from "./priority-search-pages-public-services-current";
 import { PUBLIC_SERVICE_PRIORITY_SEARCH_PAGES } from "./priority-search-pages-public-services";
 
 export function loadPrioritySearchPageServer(slug: string) {
-  return CURRENT_PUBLIC_SERVICE_PRIORITY_SEARCH_PAGES[slug]
+  return EDUCATION_PRIORITY_SEARCH_PAGES[slug]
+    ?? CURRENT_PUBLIC_SERVICE_PRIORITY_SEARCH_PAGES[slug]
     ?? PUBLIC_SERVICE_PRIORITY_SEARCH_PAGES[slug]
     ?? PRIORITY_SEARCH_PAGES[slug]
     ?? null;
