a41346141b8a1972d65adacce40258e3a96718ed
keeptxred <freddy.coppola@gmail.com>
2026-09-18 13:22:20 -0500

    Add Ward County Museum authority coverage

--- src/data/museum-expansion-statewide-wave69.ts
+++ src/data/museum-expansion-statewide-wave69.ts
@@ -1,4 +1,5 @@
 import { DESTINATION_PHOTO_PLACEHOLDER } from "./explore-hero-reconciliation";
+import { statewideMuseumExpansionWave70Destinations } from "./museum-expansion-statewide-wave70";
 import type { Destination, ImageRef } from "./types";
 
 const SOURCE_CHECKED_AT = "2026-09-18";
@@ -54,4 +55,5 @@ export const statewideMuseumExpansionWave69Destinations: Destination[] = [
     address: "100 E Broadway St, Winnsboro, TX 75494",
     sourceCheckedAt: SOURCE_CHECKED_AT,
   },
+  ...statewideMuseumExpansionWave70Destinations,
 ];
