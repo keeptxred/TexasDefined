64b312bcff2baf992ae261e3f5b75809556add76
keeptxred <freddy.coppola@gmail.com>
2026-09-22 14:36:03 -0500

    Expose campground planning details in finder

--- src/data/camping/profiles-wave5.ts
+++ src/data/camping/profiles-wave5.ts
@@ -135,6 +135,7 @@ export const CAMPING_DISCOVERY_PROFILES_WAVE5: CampgroundDiscoveryProfile[] = CA
   amenities: profile.amenities,
   reservationPolicy: profile.reservationPolicy,
   whyCampHere: profile.campingNotes?.[0],
+  planningDetail: profile.campingNotes?.[1],
   reservationUrl: profile.reservationUrl,
   siteLengthNote: profile.siteLengthNote,
   generatorRules: profile.generatorRules,
