05ed46f02ae6be3d7cdabc28ea8a8d6009c0e49d
keeptxred <freddy.coppola@gmail.com>
2026-09-23 16:55:15 -0500

    Replay wave-2 fishing change: src/data/fishing/sitemap.ts

--- src/data/fishing/sitemap.ts
+++ src/data/fishing/sitemap.ts
@@ -2,14 +2,15 @@ import { LAKE_CONROE_SECTION_SLUGS, LAKE_CONROE_VERIFIED_AT, lakeConroeCanonical
 import { FISHING_LAKE_COMPARE_PATH, FISHING_PLANNER_VERIFIED_AT, FISHING_TRIP_PLANNER_PATH } from "./planner-routing";
 import { FISHING_REGULATIONS_PATH, FISHING_REGULATIONS_VERIFIED_AT } from "./regulations-routing";
 import { FISHING_SEASONS_PATH, FISHING_SEASONS_VERIFIED_AT } from "./season-routing";
-import { EXPANDED_SHOWCASE_LAKE_SLUGS, SHOWCASE_LAKE_SECTION_SLUGS, SHOWCASE_LAKE_SLUGS, SHOWCASE_LAKE_VERIFIED_AT, showcaseLakeCanonicalPath } from "./showcase-lake-routing";
+import { EXPANDED_SHOWCASE_LAKE_SLUGS, SHOWCASE_LAKE_SECTION_SLUGS, SHOWCASE_LAKE_SLUGS, SHOWCASE_LAKE_VERIFIED_AT, WAVE2_SHOWCASE_LAKE_SLUGS, WAVE2_SHOWCASE_LAKE_VERIFIED_AT, showcaseLakeCanonicalPath } from "./showcase-lake-routing";
 import { COMPLETE_FISHING_SPECIES_SLUGS } from "./slugs";
 import { FISHING_SPECIES_DIRECTORY_PATH, FISHING_SPECIES_VERIFIED_AT, fishingSpeciesCanonicalPath } from "./species-routing";
 import { FISHING_TECHNIQUES_DIRECTORY_PATH, FISHING_TECHNIQUES_VERIFIED_AT, PUBLISHED_FISHING_TECHNIQUE_SLUGS, fishingTechniqueCanonicalPath } from "./technique-routing";
 
 export const FISHING_LAKES_DIRECTORY_PATH = "/fishing/lakes";
-export const FISHING_LAKES_DIRECTORY_VERIFIED_AT = SHOWCASE_LAKE_VERIFIED_AT;
-const ALL_SHOWCASE_LAKE_SLUGS = [...SHOWCASE_LAKE_SLUGS, ...EXPANDED_SHOWCASE_LAKE_SLUGS] as const;
+export const FISHING_LAKES_DIRECTORY_VERIFIED_AT = WAVE2_SHOWCASE_LAKE_VERIFIED_AT;
+const ALL_SHOWCASE_LAKE_SLUGS = [...SHOWCASE_LAKE_SLUGS, ...EXPANDED_SHOWCASE_LAKE_SLUGS, ...WAVE2_SHOWCASE_LAKE_SLUGS] as const;
+const WAVE2_SET = new Set<string>(WAVE2_SHOWCASE_LAKE_SLUGS);
 
 // FISHING_GUIDES_DIRECTORY_PATH, FISHING_REPORTS_DIRECTORY_PATH,
 // FISHING_ACCESS_DIRECTORY_PATH and FISHING_SERVICES_DIRECTORY_PATH are conditional:
@@ -25,8 +26,8 @@ export const FISHING_SITEMAP_ENTRIES = [
   { path: lakeConroeCanonicalPath(), lastmod: LAKE_CONROE_VERIFIED_AT },
   ...LAKE_CONROE_SECTION_SLUGS.map((section) => ({ path: lakeConroeCanonicalPath(section), lastmod: LAKE_CONROE_VERIFIED_AT })),
   ...ALL_SHOWCASE_LAKE_SLUGS.flatMap((slug) => [
-    { path: showcaseLakeCanonicalPath(slug), lastmod: SHOWCASE_LAKE_VERIFIED_AT },
-    ...SHOWCASE_LAKE_SECTION_SLUGS.map((section) => ({ path: showcaseLakeCanonicalPath(slug, section), lastmod: SHOWCASE_LAKE_VERIFIED_AT })),
+    { path: showcaseLakeCanonicalPath(slug), lastmod: WAVE2_SET.has(slug) ? WAVE2_SHOWCASE_LAKE_VERIFIED_AT : SHOWCASE_LAKE_VERIFIED_AT },
+    ...SHOWCASE_LAKE_SECTION_SLUGS.map((section) => ({ path: showcaseLakeCanonicalPath(slug, section), lastmod: WAVE2_SET.has(slug) ? WAVE2_SHOWCASE_LAKE_VERIFIED_AT : SHOWCASE_LAKE_VERIFIED_AT })),
   ]),
   { path: FISHING_SPECIES_DIRECTORY_PATH, lastmod: FISHING_SPECIES_VERIFIED_AT },
   ...COMPLETE_FISHING_SPECIES_SLUGS.map((slug) => ({ path: fishingSpeciesCanonicalPath(slug), lastmod: FISHING_SPECIES_VERIFIED_AT })),
