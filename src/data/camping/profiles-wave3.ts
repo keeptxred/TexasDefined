e23ac70f712e60ab665bab848fd9145586f277a2
keeptxred <freddy.coppola@gmail.com>
2026-09-23 16:00:34 -0500

    Replay validated camping search and schema fix

--- src/data/camping/profiles-wave3.ts
+++ src/data/camping/profiles-wave3.ts
@@ -231,6 +231,7 @@ export const CAMPING_DISCOVERY_PROFILES_WAVE3: CampgroundDiscoveryProfile[] = CA
   reservationPolicy: profile.reservationPolicy,
   whyCampHere: profile.campingNotes?.[0],
   planningDetail: profile.campingNotes?.[1],
+  searchTerms: profile.searchTerms,
   reservationUrl: profile.reservationUrl,
   siteLengthNote: profile.siteLengthNote,
   generatorRules: profile.generatorRules,
