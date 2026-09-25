efd812a769b84195443555736e16003197e37d8e
keeptxred <freddy.coppola@gmail.com>
2026-09-25 08:26:47 -0500

    Add shared financial calculation engine

---
+++ src/lib/financial/index.ts
@@ -0,0 +1,6 @@
+export * from './validation.ts';
+export * from './mortgage.ts';
+export * from './affordability.ts';
+export * from './closing-costs.ts';
+export * from './homeownership.ts';
+export * from './planning.ts';
